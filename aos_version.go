0c4a46dd66caf091038d1c5a9f9bf1c0e96952cb
Chris Marget <cmarget@juniper.net>
2022-04-28 18:38:49 -0400

    add aos version, refactor client get/post methods

---
+++ aos_version.go
@@ -0,0 +1,25 @@
+package apstraTelemetry
+
+import (
+	"fmt"
+)
+
+const (
+	aosApiVersion = "/api/version"
+)
+
+type AosVersionResponse struct {
+	Major   string `json:"major"`
+	Version string `json:"version"`
+	Build   string `json:"build"`
+	Minor   string `json:"minor"`
+}
+
+func (o AosClient) GetVersion() (*AosVersionResponse, error) {
+	var versionResponse AosVersionResponse
+	err := o.newGet(o.baseUrl+aosApiVersion, []int{200}, &versionResponse)
+	if err != nil {
+		return nil, fmt.Errorf("error calling AosClient.get() - %v", err)
+	}
+	return &versionResponse, nil
+}
