f8bf3554fddc100c6ef29f5658f645c4403561ff
Chris Marget <cmarget@juniper.net>
2022-05-13 10:14:22 -0400

    move Client.login() and Client.logout() to api_user.go

--- apstra/api_user.go
+++ apstra/api_user.go
@@ -1,5 +1,12 @@
 package apstra
 
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
 const (
 	apiUrlUserLogin  = "/api/user/login"
 	apiUrlUserLogout = "/api/user/logout"
@@ -15,4 +22,48 @@ type userLoginRequest struct {
 type userLoginResponse struct {
 	Token string `json:"token"`
 	Id    string `json:"id"`
+}
+
+func (o *Client) login(ctx context.Context) error {
+	apstraUrl, err := url.Parse(apiUrlUserLogin)
+	if err != nil {
+		return fmt.Errorf("error parsing url '%s' - %w", apiUrlUserLogin, err)
+	}
+	response := &userLoginResponse{}
+	err = o.talkToApstra(ctx, &talkToApstraIn{
+		method: http.MethodPost,
+		url:    apstraUrl,
+		apiInput: &userLoginRequest{
+			Username: o.cfg.User,
+			Password: o.cfg.Pass,
+		},
+		doNotLogin:  true,
+		apiResponse: response,
+	})
+	if err != nil {
+		return fmt.Errorf("error talking to AOS in Login - %w", err)
+	}
+
+	// stash auth token in client's default set of apstra http httpHeaders
+	o.httpHeaders[apstraAuthHeader] = response.Token
+
+	return nil
+}
+
+func (o Client) logout(ctx context.Context) error {
+	defer close(o.tmQuit) // shut down the task monitor gothread
+
+	apstraUrl, err := url.Parse(apiUrlUserLogout)
+	if err != nil {
+		return fmt.Errorf("error parsing url '%s' - %w", apiUrlUserLogout, err)
+	}
+	err = o.talkToApstra(ctx, &talkToApstraIn{
+		method: http.MethodPost,
+		url:    apstraUrl,
+	})
+	if err != nil {
+		return fmt.Errorf("error calling '%s' - %w", apiUrlUserLogout, err)
+	}
+	delete(o.httpHeaders, apstraAuthHeader)
+	return nil
 }
--- apstra/client.go
+++ apstra/client.go
@@ -110,53 +110,9 @@ func (o *Client) Login(ctx context.Context) error {
 	return o.login(ctx)
 }
 
-func (o *Client) login(ctx context.Context) error {
-	apstraUrl, err := url.Parse(apiUrlUserLogin)
-	if err != nil {
-		return fmt.Errorf("error parsing url '%s' - %w", apiUrlUserLogin, err)
-	}
-	response := &userLoginResponse{}
-	err = o.talkToApstra(ctx, &talkToApstraIn{
-		method: http.MethodPost,
-		url:    apstraUrl,
-		apiInput: &userLoginRequest{
-			Username: o.cfg.User,
-			Password: o.cfg.Pass,
-		},
-		doNotLogin:  true,
-		apiResponse: response,
-	})
-	if err != nil {
-		return fmt.Errorf("error talking to AOS in Login - %w", err)
-	}
-
-	// stash auth token in client's default set of apstra http httpHeaders
-	o.httpHeaders[apstraAuthHeader] = response.Token
-
-	return nil
-}
-
 // Logout invalidates the Apstra API token held by Client
 func (o Client) Logout(ctx context.Context) error {
 	return o.logout(ctx)
-}
-
-func (o Client) logout(ctx context.Context) error {
-	defer close(o.tmQuit) // shut down the task monitor gothread
-
-	apstraUrl, err := url.Parse(apiUrlUserLogout)
-	if err != nil {
-		return fmt.Errorf("error parsing url '%s' - %w", apiUrlUserLogout, err)
-	}
-	err = o.talkToApstra(ctx, &talkToApstraIn{
-		method: http.MethodPost,
-		url:    apstraUrl,
-	})
-	if err != nil {
-		return fmt.Errorf("error calling '%s' - %w", apiUrlUserLogout, err)
-	}
-	delete(o.httpHeaders, apstraAuthHeader)
-	return nil
 }
 
 // functions below here are implemented in other files.
