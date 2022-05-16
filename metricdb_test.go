839c56468f2b38d855d289d70cdc4185ea8e3318
Chris Marget <cmarget@juniper.net>
2022-05-15 20:45:39 -0400

    begin metricdb

--- metricdb_test.go
+++ metricdb_test.go
@@ -80,6 +80,65 @@ const (
     ]}`
 )
 
+//      "application": "cluster_health_info",
+//      "namespace": "agent",
+//      "name": "health_aggr_3600",
+// Empty
+
+//
+//      "application": "cluster_health_info",
+//      "namespace": "file_registry",
+//      "name": "file_aggr_3600"
+//  {   "node": "AosController",
+//      "timestamp": "2022-05-13T00:44:09.723419Z",
+//      "file_path": "/var/lib/aos/metricdb/cluster_health_info/node/disk_utilization/disk_utilization-189-2022-05-12--16-45-55.616443.tel",
+//      "size": 34535 }
+
+//
+//      "application": "iba",
+//      "namespace": "db10754a-610e-475b-9baa-4c85f82282e8/d6ffca4d-ba91-4833-bf43-714bc0c5b665",
+//      "name": "imbalanced_system_count_out_of_range",
+// Empty
+
+//
+//      "application": "cluster_health_info",
+//      "namespace": "file_registry",
+//      "name": "directory_aggr_3600",
+//  {   "node": "AosController",
+//      "timestamp": "2022-05-13T00:44:09.730437Z",
+//      "size": 78081287,
+//      "directory_path": "/var/lib/aos/metricdb/cluster_health_info" }
+
+//
+//      "application": "cluster_health_info",
+//      "namespace": "agent",
+//      "name": "health",
+// Empty
+
+//
+//      "application": "cluster_health_info",
+//      "namespace": "agent",
+//      "name": "utilization",
+//  {   "node": "AosController",
+//      "container": "aos_sysdb_1",
+//      "timestamp": "2022-05-13T00:00:00.186722Z",
+//      "agent": "CognacSysdb",
+//      "memory": 151576576,
+//      "cpu": 0 }
+
+//
+//      "application": "iba",
+//      "namespace": "db10754a-610e-475b-9baa-4c85f82282e8/4eb11184-4b32-4106-8e90-edb312042683",
+//      "name": "System Interface Counters"
+//  {   "timestamp": "2022-05-14T00:00:01.367573Z",
+//      "aggregate_rx_bps": 1376,
+//      "max_ifc_rx_utilization": 0,
+//      "max_ifc_tx_utilization": 0,
+//      "system_id": "WS3119350041",
+//      "aggregate_rx_utilization": 0,
+//      "aggregate_tx_bps": 1500,
+//      "aggregate_tx_utilization": 0 }
+
 func (o *mockApstraApi) createMetricdb() error {
 	return json.Unmarshal([]byte(mockGetMetricdbPayload), &o.metricDb)
 }
