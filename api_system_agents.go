62a2c32a58e4114afa0fdba78215c53a2f863fa8
Chris Marget <cmarget@juniper.net>
2022-08-12 11:38:01 -0400

    move agent packages stuff into api_system_agents.go; change signature to eliminate pointer return on raw()

--- api_system_agents.go
+++ api_system_agents.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
 
@@ -372,6 +373,41 @@ func (o rawAgentCxnState) parse() int {
 	default:
 		return int(AgentCxnStateUnknown)
 	}
+}
+
+type AgentPackages map[string]string
+
+// todo: method for inventory
+//   to be checked by agent profile and agent create/update methods,
+//   refuse to reference packages which do not exist.
+
+func (o *AgentPackages) raw() rawAgentPackages {
+	// todo: one of these lines causes 'null' in JSON output, while the other causes '[]' ... which one is correct for the API?
+	//raw := rawAgentPackages{}
+	var raw rawAgentPackages
+	for k, v := range *o {
+		raw = append(raw, k+apstraSystemAgentPlatformStringSep+v)
+	}
+	return raw
+}
+
+type rawAgentPackages []string
+
+func (o rawAgentPackages) polish() AgentPackages {
+	var polish AgentPackages
+	if len(o) > 0 {
+		polish = make(map[string]string)
+	}
+	for _, s := range o {
+		kv := strings.SplitN(s, apstraSystemAgentPlatformStringSep, 2)
+		switch len(kv) {
+		case 2:
+			polish[kv[0]] = kv[1]
+		case 1:
+			polish[kv[0]] = ""
+		}
+	}
+	return polish
 }
 
 type agentJobHistoryResponse struct {
