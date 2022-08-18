cbda6b7d3c0783075da2a38332cc7c882d480547
Chris Marget <cmarget@juniper.net>
2022-08-18 15:19:32 -0400

    convert blueprints + security_zones tests to use cloudlabs

--- api_blueprints_test.go
+++ api_blueprints_test.go
@@ -2,142 +2,148 @@ package goapstra
 
 import (
 	"context"
-	"crypto/tls"
 	"encoding/json"
 	"log"
 	"testing"
 )
 
-func blueprintsTestClient1() (*Client, error) {
-	return NewClient(&ClientCfg{
-		TlsConfig: &tls.Config{InsecureSkipVerify: true},
-	})
-}
-
 func TestListAllBlueprintIds(t *testing.T) {
-	client, err := blueprintsTestClient1()
+	clients, err := getCloudlabsTestClients()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	blueprints, err := client.listAllBlueprintIds(context.TODO())
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, client := range clients {
+		log.Printf("testing listAllBlueprintIds() against Apsra %s\n", client.ApiVersion())
+		blueprints, err := client.listAllBlueprintIds(context.TODO())
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	result, err := json.Marshal(blueprints)
-	if err != nil {
-		t.Fatal(err)
+		result, err := json.Marshal(blueprints)
+		if err != nil {
+			t.Fatal(err)
+		}
+		log.Println(string(result))
 	}
-	log.Println(string(result))
 }
 
 func TestGetAllBlueprintStatus(t *testing.T) {
-	client, err := blueprintsTestClient1()
+	clients, err := getCloudlabsTestClients()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	bps, err := client.getAllBlueprintStatus(context.TODO())
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, client := range clients {
+		log.Printf("testing getAllBlueprintStatus() against Apstra %s\n", client.ApiVersion())
+		bps, err := client.getAllBlueprintStatus(context.TODO())
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	log.Println(len(bps))
+		log.Println(len(bps))
+	}
 }
 
 func TestCreateDeleteBlueprint(t *testing.T) {
-	client, err := blueprintsTestClient1()
+	clients, err := getCloudlabsTestClients()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	client.Login(context.TODO())
-	name := randString(10, "hex")
-	id, err := client.createBlueprintFromTemplate(context.TODO(), &CreateBlueprintFromTemplate{
-		RefDesign:  RefDesignDatacenter,
-		Label:      name,
-		TemplateId: "L2_Virtual_EVPN",
-	})
+	for _, client := range clients {
+		log.Printf("testing createBlueprintFromTemplate() against Apstra %s\n", client.ApiVersion())
+		name := randString(10, "hex")
+		id, err := client.createBlueprintFromTemplate(context.TODO(), &CreateBlueprintFromTemplate{
+			RefDesign:  RefDesignDatacenter,
+			Label:      name,
+			TemplateId: "L2_Virtual_EVPN",
+		})
 
-	log.Printf("got id '%s'\n", id)
-
-	err = client.deleteBlueprint(context.TODO(), id)
-	if err != nil {
-		log.Fatal(err)
+		log.Printf("got id '%s', deleting blueprint...\n", id)
+		err = client.deleteBlueprint(context.TODO(), id)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
 func TestGetPatchGetPatchNode(t *testing.T) {
-	client, err := newLiveTestClient()
+	clients, err := getCloudlabsTestClients()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	bpIds, err := client.listAllBlueprintIds(context.TODO())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(bpIds) == 0 {
-		t.Skip("no blueprints? no nodes.")
-	}
-
-	type metadataNode struct {
-		Tags         interface{} `json:"tags,omitempty"`
-		PropertySet  interface{} `json:"property_set,omitempty"`
-		Label        string      `json:"label,omitempty"`
-		UserIp       interface{} `json:"user_ip,omitempty"`
-		TemplateJson interface{} `json:"template_json,omitempty"`
-		Design       string      `json:"design,omitempty"`
-		User         interface{} `json:"user,omitempty"`
-		Type         string      `json:"type,omitempty"`
-		Id           ObjectId    `json:"id,omitempty"`
-	}
-
-	nodesA := struct {
-		Nodes map[string]metadataNode `json:"nodes"`
-	}{}
-	nodesB := struct {
-		Nodes map[string]metadataNode `json:"nodes"`
-	}{}
-	err = client.getNodes(context.TODO(), bpIds[0], NodeTypeMetadata, &nodesA)
-	if err != nil {
-		t.Fatal()
-	}
-
-	if len(nodesA.Nodes) != 1 {
-		t.Fatalf("not expecting %d '%s' nodes", len(nodesA.Nodes), NodeTypeMetadata)
-	}
-
-	newName := randString(10, "hex")
-	// loop should run just once (len check above)
-	for idA, nodeA := range nodesA.Nodes {
-		log.Printf("node id: %s ; label: %s\n", idA, nodeA.Label)
-
-		req := metadataNode{Label: newName}
-		resp := &metadataNode{}
-		err := client.patchNode(context.TODO(), bpIds[0], nodeA.Id, req, resp)
+	for _, client := range clients {
+		log.Printf("testing listAllBlueprintIds() against Apstra %s\n", client.ApiVersion())
+		bpIds, err := client.listAllBlueprintIds(context.TODO())
 		if err != nil {
 			t.Fatal(err)
 		}
-		log.Printf("response indicates name changed '%s' -> '%s'", nodeA.Label, resp.Label)
 
-		err = client.getNodes(context.TODO(), bpIds[0], NodeTypeMetadata, &nodesB)
+		if len(bpIds) == 0 {
+			t.Skip("no blueprints? no nodes.")
+		}
+
+		type metadataNode struct {
+			Tags         interface{} `json:"tags,omitempty"`
+			PropertySet  interface{} `json:"property_set,omitempty"`
+			Label        string      `json:"label,omitempty"`
+			UserIp       interface{} `json:"user_ip,omitempty"`
+			TemplateJson interface{} `json:"template_json,omitempty"`
+			Design       string      `json:"design,omitempty"`
+			User         interface{} `json:"user,omitempty"`
+			Type         string      `json:"type,omitempty"`
+			Id           ObjectId    `json:"id,omitempty"`
+		}
+
+		nodesA := struct {
+			Nodes map[string]metadataNode `json:"nodes"`
+		}{}
+		nodesB := struct {
+			Nodes map[string]metadataNode `json:"nodes"`
+		}{}
+		log.Printf("testing getNodes() against Apstra %s\n", client.ApiVersion())
+		err = client.getNodes(context.TODO(), bpIds[0], NodeTypeMetadata, &nodesA)
 		if err != nil {
 			t.Fatal()
 		}
-		for idB, nodeB := range nodesB.Nodes {
-			log.Printf("node id: %s ; label: %s\n", idB, nodeB.Label)
 
+		if len(nodesA.Nodes) != 1 {
+			t.Fatalf("not expecting %d '%s' nodes", len(nodesA.Nodes), NodeTypeMetadata)
 		}
 
-		req = metadataNode{Label: nodeA.Label}
-		err = client.patchNode(context.TODO(), bpIds[0], nodeA.Id, req, resp)
-		if err != nil {
-			t.Fatal(err)
+		newName := randString(10, "hex")
+		// loop should run just once (len check above)
+		for idA, nodeA := range nodesA.Nodes {
+			log.Printf("node id: %s ; label: %s\n", idA, nodeA.Label)
+
+			req := metadataNode{Label: newName}
+			resp := &metadataNode{}
+			log.Printf("testing patchNode() against Apstra %s\n", client.ApiVersion())
+			err := client.patchNode(context.TODO(), bpIds[0], nodeA.Id, req, resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			log.Printf("response indicates name changed '%s' -> '%s'", nodeA.Label, resp.Label)
+
+			log.Printf("testing getNodes() against Apstra %s\n", client.ApiVersion())
+			err = client.getNodes(context.TODO(), bpIds[0], NodeTypeMetadata, &nodesB)
+			if err != nil {
+				t.Fatal()
+			}
+			for idB, nodeB := range nodesB.Nodes {
+				log.Printf("node id: %s ; label: %s\n", idB, nodeB.Label)
+
+			}
+
+			req = metadataNode{Label: nodeA.Label}
+			log.Printf("testing patchNode() against Apstra %s\n", client.ApiVersion())
+			err = client.patchNode(context.TODO(), bpIds[0], nodeA.Id, req, resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			log.Printf("response indicates name changed '%s' -> '%s'", newName, resp.Label)
 		}
-		log.Printf("response indicates name changed '%s' -> '%s'", newName, resp.Label)
 	}
-
 }
--- two_stage_l3_clos_security_zones_test.go
+++ two_stage_l3_clos_security_zones_test.go
@@ -7,114 +7,127 @@ import (
 )
 
 func TestCreateUpdateDeleteRoutingZone(t *testing.T) {
-	client, err := blueprintsTestClient1()
+	clients, err := getCloudlabsTestClients()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	blueprints, err := client.listAllBlueprintIds(context.TODO())
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, client := range clients {
+		log.Printf("testing listAllBlueprintIds() against Apstra %s\n", client.ApiVersion())
+		blueprints, err := client.listAllBlueprintIds(context.TODO())
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if len(blueprints) == 0 {
-		t.Skipf("cannot proceed without at least one blueprint")
-	}
+		if len(blueprints) == 0 {
+			t.Skipf("cannot proceed without at least one blueprint")
+		}
 
-	dcClient, err := client.NewTwoStageL3ClosClient(context.TODO(), blueprints[0])
-	if err != nil {
-		t.Fatal(err)
-	}
+		log.Printf("testing NewTwoStageL3ClosClient() against Apstra %s\n", client.ApiVersion())
+		dcClient, err := client.NewTwoStageL3ClosClient(context.TODO(), blueprints[0])
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	randStr := randString(5, "hex")
+		randStr := randString(5, "hex")
 
-	label := "test-" + randStr
-	vrfName := "test-" + randStr
-	zoneId, err := dcClient.CreateSecurityZone(context.TODO(), &CreateSecurityZoneCfg{
-		SzType:  "evpn",
-		VrfName: vrfName,
-		Label:   label,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
+		label := "test-" + randStr
+		vrfName := "test-" + randStr
+		log.Printf("testing CreateSecurityZone() against Apstra %s\n", dcClient.client.ApiVersion())
+		zoneId, err := dcClient.CreateSecurityZone(context.TODO(), &CreateSecurityZoneCfg{
+			SzType:  "evpn",
+			VrfName: vrfName,
+			Label:   label,
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	log.Printf("created zone - id:'%s', name: '%s', label:'%s'", zoneId, vrfName, label)
+		log.Printf("created zone - id:'%s', name: '%s', label:'%s'", zoneId, vrfName, label)
 
-	log.Println("fetching by id...")
-	zone, err := dcClient.getSecurityZone(context.TODO(), zoneId)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if zone.Id != zoneId {
-		t.Fatalf("created vs. fetched zone IDs don't match: '%s' and '%s'", zone.Id, zoneId)
-	}
+		log.Println("fetching by id...")
+		log.Printf("testing getSecurityZone() against Apstra %s\n", dcClient.client.ApiVersion())
+		zone, err := dcClient.getSecurityZone(context.TODO(), zoneId)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if zone.Id != zoneId {
+			t.Fatalf("created vs. fetched zone IDs don't match: '%s' and '%s'", zone.Id, zoneId)
+		}
 
-	log.Println("fetching by vrf name...")
-	zone, err = dcClient.getSecurityZoneByName(context.TODO(), vrfName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if zone.Id != zoneId {
-		t.Fatalf("created vs. fetched zone IDs don't match: '%s' and '%s'", zone.Id, zoneId)
-	}
+		log.Println("fetching by vrf name...")
+		log.Printf("testing getSecurityZoneByName() against Apstra %s\n", dcClient.client.ApiVersion())
+		zone, err = dcClient.getSecurityZoneByName(context.TODO(), vrfName)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if zone.Id != zoneId {
+			t.Fatalf("created vs. fetched zone IDs don't match: '%s' and '%s'", zone.Id, zoneId)
+		}
 
-	randStr2 := randString(5, "hex")
-	vrfName2 := "test-" + randStr2
-	label2 := "test-" + randStr2
-	err = dcClient.UpdateSecurityZone(context.TODO(), zoneId, &CreateSecurityZoneCfg{
-		SzType:  "evpn",
-		VrfName: vrfName2,
-		Label:   label2,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
+		randStr2 := randString(5, "hex")
+		vrfName2 := "test-" + randStr2
+		label2 := "test-" + randStr2
+		log.Printf("testing UpdateSecurityZone() against Apstra %s\n", dcClient.client.ApiVersion())
+		err = dcClient.UpdateSecurityZone(context.TODO(), zoneId, &CreateSecurityZoneCfg{
+			SzType:  "evpn",
+			VrfName: vrfName2,
+			Label:   label2,
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	zone, err = dcClient.GetSecurityZoneByName(context.TODO(), vrfName2)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if zone.Id != zoneId {
-		t.Fatal()
-	}
-	if zone.VrfName != vrfName2 {
-		t.Fatal()
-	}
+		log.Printf("testing GetSecurityZoneByName() against Apstra %s\n", dcClient.client.ApiVersion())
+		zone, err = dcClient.GetSecurityZoneByName(context.TODO(), vrfName2)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if zone.Id != zoneId {
+			t.Fatal()
+		}
+		if zone.VrfName != vrfName2 {
+			t.Fatal()
+		}
 
-	log.Println("deleting security zone...")
-
-	err = dcClient.DeleteSecurityZone(context.TODO(), zoneId)
-	if err != nil {
-		t.Fatal(err)
+		log.Printf("testing DeleteSecurityZone() against Apstra %s\n", dcClient.client.ApiVersion())
+		err = dcClient.DeleteSecurityZone(context.TODO(), zoneId)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 }
 
 func TestGetDefaultRoutingZone(t *testing.T) {
-	client, err := blueprintsTestClient1()
+	clients, err := getCloudlabsTestClients()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	blueprints, err := client.listAllBlueprintIds(context.TODO())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(blueprints) == 0 {
-		t.Skipf("cannot proceed without at least one blueprint")
-	}
-
-	for _, bpId := range blueprints {
-		dcClient, err := client.NewTwoStageL3ClosClient(context.TODO(), bpId)
+	for _, client := range clients {
+		log.Printf("testing listAllBlueprintIds() against Apstra %s\n", client.ApiVersion())
+		blueprints, err := client.listAllBlueprintIds(context.TODO())
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		sz, err := dcClient.GetSecurityZoneByName(context.TODO(), "default")
-		if err != nil {
-			t.Fatal(err)
+		if len(blueprints) == 0 {
+			t.Skipf("cannot proceed without at least one blueprint")
 		}
-		log.Printf("blueprint: %s - default security zone: %s", bpId, sz.Id)
+
+		for _, bpId := range blueprints {
+			log.Printf("testing NewTwoStageL3ClosClient() against Apstra %s\n", client.ApiVersion())
+			dcClient, err := client.NewTwoStageL3ClosClient(context.TODO(), bpId)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			log.Printf("testing GetSecurityZoneByName() against Apstra %s\n", dcClient.client.ApiVersion())
+			sz, err := dcClient.GetSecurityZoneByName(context.TODO(), "default")
+			if err != nil {
+				t.Fatal(err)
+			}
+			log.Printf("blueprint: %s - default security zone: %s", bpId, sz.Id)
+		}
 	}
 }
