389aff9ae50528299d22ebc6255988c2b31e0ab8
Chris Marget <cmarget@juniper.net>
2022-08-19 09:48:00 -0400

    update rack_types_test to use new clients

--- api_design_rack_types_test.go
+++ api_design_rack_types_test.go
@@ -9,64 +9,74 @@ import (
 )
 
 func TestListGetOneRackType(t *testing.T) {
-	client, err := newLiveTestClient()
+	clients, err := getTestClients()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	rtIds, err := client.listRackTypeIds(context.TODO())
-	if err != nil {
-		t.Fatal(err)
-	}
-	_ = rtIds
+	for _, client := range clients {
+		log.Printf("testing listRackTypeIds() against %s %s (%s)", client.clientType, client.clientName, client.client.ApiVersion())
+		rtIds, err := client.client.listRackTypeIds(context.TODO())
+		if err != nil {
+			t.Fatal(err)
+		}
+		_ = rtIds
 
-	rt, err := client.getRackType(context.TODO(), rtIds[0])
-	//rt, err := client.getRackType(context.TODO(), ObjectId("test-name"))
-	if err != nil {
-		t.Fatal(err)
-	}
+		log.Printf("testing getRackType() against %s %s (%s)", client.clientType, client.clientName, client.client.ApiVersion())
+		rt, err := client.client.getRackType(context.TODO(), rtIds[0])
+		//rt, err := client.getRackType(context.TODO(), ObjectId("test-name"))
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	log.Println(rt.Id)
+		log.Println(rt.Id)
+	}
 
 }
 
 func TestListGetAllGetRackType(t *testing.T) {
-	client, err := newLiveTestClient()
+	clients, err := getTestClients()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	rackTypeIds, err := client.listRackTypeIds(context.TODO())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for _, id := range rackTypeIds {
-		rt, err := client.getRackType(context.TODO(), id)
+	for _, client := range clients {
+		log.Printf("testing listRackTypeIds() against %s %s (%s)", client.clientType, client.clientName, client.client.ApiVersion())
+		rackTypeIds, err := client.client.listRackTypeIds(context.TODO())
 		if err != nil {
 			t.Fatal(err)
 		}
-		log.Println(rt.Id)
-	}
 
-	rackTypes, err := client.getAllRackTypes(context.TODO())
-	if err != nil {
-		t.Fatal(err)
-	}
+		for _, id := range rackTypeIds {
+			log.Printf("testing getRackType() against %s %s (%s)", client.clientType, client.clientName, client.client.ApiVersion())
+			rt, err := client.client.getRackType(context.TODO(), id)
+			if err != nil {
+				t.Fatal(err)
+			}
+			log.Println(rt.Id)
+		}
 
-	if len(rackTypeIds) != len(rackTypes) {
-		t.Fatalf("got %d rack type IDs but %d rack types", len(rackTypeIds), len(rackTypes))
-	}
+		log.Printf("testing getAllRackTypes() against %s %s (%s)", client.clientType, client.clientName, client.client.ApiVersion())
+		rackTypes, err := client.client.getAllRackTypes(context.TODO())
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	rand.Seed(time.Now().UnixNano())
-	randRackid := rackTypeIds[rand.Intn(len(rackTypeIds))]
-	rt, err := client.getRackType(context.TODO(), randRackid)
-	if err != nil {
-		t.Fatal(err)
-	}
+		if len(rackTypeIds) != len(rackTypes) {
+			t.Fatalf("got %d rack type IDs but %d rack types", len(rackTypeIds), len(rackTypes))
+		}
 
-	log.Printf("randomly selected rack type '%s' (%s) has %d leaf switches, %d access switches, and %d generic systems",
-		rt.DisplayName, rt.Id, len(rt.LeafSwitches), len(rt.AccessSwitches), len(rt.GenericSystems))
+		rand.Seed(time.Now().UnixNano())
+		randRackid := rackTypeIds[rand.Intn(len(rackTypeIds))]
+		log.Printf("testing getRackType() against %s %s (%s)", client.clientType, client.clientName, client.client.ApiVersion())
+		rt, err := client.client.getRackType(context.TODO(), randRackid)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		log.Printf("randomly selected rack type '%s' (%s) has %d leaf switches, %d access switches, and %d generic systems",
+			rt.DisplayName, rt.Id, len(rt.LeafSwitches), len(rt.AccessSwitches), len(rt.GenericSystems))
+	}
 }
 
 func TestRackTypeStrings(t *testing.T) {
@@ -123,57 +133,64 @@ func TestRackTypeStrings(t *testing.T) {
 }
 
 func TestCreateGetRackDeleteRackType(t *testing.T) {
-	client, err := newLiveTestClient()
+	clients, err := getTestClients()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	leafLabel := "ll-" + randString(10, "hex")
+	for _, client := range clients {
+		leafLabel := "ll-" + randString(10, "hex")
 
-	id, err := client.CreateRackType(context.TODO(), &RackTypeRequest{
-		DisplayName:              "rdn " + randString(5, "hex"),
-		FabricConnectivityDesign: FabricConnectivityDesignL3Clos,
-		LeafSwitches: []RackElementLeafSwitchRequest{
-			{
-				Label:             leafLabel,
-				LogicalDeviceId:   "virtual-7x10-1",
-				LinkPerSpineCount: 2,
-				LinkPerSpineSpeed: "10G",
-			},
-		},
-		GenericSystems: []RackElementGenericSystemRequest{
-			{
-				Count: 5,
-				Label: "some generic system",
-				Links: []RackLinkRequest{
-					{
-						Label:              "foo",
-						LinkPerSwitchCount: 1,
-						LinkSpeed:          "10G",
-						TargetSwitchLabel:  leafLabel,
-						AttachmentType:     RackLinkAttachmentTypeSingle,
-						LagMode:            RackLinkLagModeNone,
-						Tags:               []TagLabel{"my_tag"},
-					},
+		log.Printf("testing CreateRackType() against %s %s (%s)", client.clientType, client.clientName, client.client.ApiVersion())
+		rtr := &RackTypeRequest{
+			DisplayName:              "rdn " + randString(5, "hex"),
+			FabricConnectivityDesign: FabricConnectivityDesignL3Clos,
+			LeafSwitches: []RackElementLeafSwitchRequest{
+				{
+					Label:             leafLabel,
+					LogicalDeviceId:   "virtual-7x10-1",
+					LinkPerSpineCount: 2,
+					LinkPerSpineSpeed: "10G",
 				},
-				LogicalDeviceId: "5ed7ed07-7222-4d6c-a5cb-1e1aa6036dab",
 			},
-		},
-		AccessSwitches: nil,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
+			GenericSystems: []RackElementGenericSystemRequest{
+				{
+					Count: 5,
+					Label: "some generic system",
+					Links: []RackLinkRequest{
+						{
+							Label:              "foo",
+							LinkPerSwitchCount: 1,
+							LinkSpeed:          "10G",
+							TargetSwitchLabel:  leafLabel,
+							AttachmentType:     RackLinkAttachmentTypeSingle,
+							LagMode:            RackLinkLagModeNone,
+							Tags:               []TagLabel{"Hypervisor"},
+						},
+					},
+					LogicalDeviceId: "AOS-1x10-1",
+				},
+			},
+			AccessSwitches: nil,
+		}
 
-	rt, err := client.GetRackType(context.TODO(), id)
-	if err != nil {
-		t.Fatal(err)
-	}
+		id, err := client.client.CreateRackType(context.TODO(), rtr)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	err = client.DeleteRackType(context.TODO(), id)
-	if err != nil {
-		t.Fatal(err)
-	}
+		log.Printf("testing GetRackType() against %s %s (%s)", client.clientType, client.clientName, client.client.ApiVersion())
+		rt, err := client.client.GetRackType(context.TODO(), id)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	log.Printf("id: '%s'\n", rt.Id)
+		log.Printf("testing DeleteRackType() against %s %s (%s)", client.clientType, client.clientName, client.client.ApiVersion())
+		err = client.client.DeleteRackType(context.TODO(), id)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		log.Printf("id: '%s'\n", rt.Id)
+	}
 }
