cc190982392d42fffeb2ee452bd5632a701b67fd
Chris Marget <cmarget@juniper.net>
2022-08-19 21:30:20 -0400

    remove mock features

--- api_virtual_infra_manager_test.go
+++ api_virtual_infra_manager_test.go
@@ -7,22 +7,6 @@ import (
 	"testing"
 )
 
-const (
-	mockVirtualInfraMgrInfo = `{
-      "items": [
-        {
-          "connection_state": "connected",
-          "last_successful_collection_time": "2022-05-17T20:36:00.712641Z",
-          "service_enabled": true,
-          "management_ip": "100.123.91.106",
-          "system_id": "382f335c-13cc-47b1-8591-7f90d92a20bc",
-          "agent_id": "6ec9ac7d-bde2-48b6-9d01-71fd815531a6",
-          "virtual_infra_type": "vcenter"
-        }
-      ]
-    }`
-)
-
 func TestGetVirtualInfraMgrs(t *testing.T) {
 	clients, err := getTestClients()
 	if err != nil {
