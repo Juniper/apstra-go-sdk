Juniper/apstra-go-sdk

commit 9892c4ede5e2e5139e05b40c36cfe9422d596f18

rawLogicalDevicePortSpeed->*rawLogicalDevicePortSpeed to send 'null' for unused fields

api_design_rack_types.go
@@ -469,19 +469,19 @@ type RackElementLeafSwitch struct {
 }
 
 type rawRackElementLeafSwitch struct {
-	Label                       string                    `json:"label"`
-	LeafLeafL3LinkCount         int                       `json:"leaf_leaf_l3_link_count"`
-	LeafLeafL3LinkPortChannelId int                       `json:"leaf_leaf_l3_link_port_channel_id"`
-	LeafLeafL3LinkSpeed         rawLogicalDevicePortSpeed `json:"leaf_leaf_l3_link_speed"`
-	LeafLeafLinkCount           int                       `json:"leaf_leaf_link_count"`
-	LeafLeafLinkPortChannelId   int                       `json:"leaf_leaf_link_port_channel_id"`
-	LeafLeafLinkSpeed           rawLogicalDevicePortSpeed `json:"leaf_leaf_link_speed"`
-	LinkPerSpineCount           int                       `json:"link_per_spine_count"`
-	LinkPerSpineSpeed           rawLogicalDevicePortSpeed `json:"link_per_spine_speed"`
-	LogicalDevice               ObjectId                  `json:"logical_device"`
-	MlagVlanId                  int                       `json:"mlag_vlan_id"`
-	RedundancyProtocol          leafRedundancyProtocol    `json:"redundancy_protocol,omitempty"`
-	Tags                        []TagLabel                `json:"tags"`
+	Label                       string                     `json:"label"`
+	LeafLeafL3LinkCount         int                        `json:"leaf_leaf_l3_link_count"`
+	LeafLeafL3LinkPortChannelId int                        `json:"leaf_leaf_l3_link_port_channel_id"`
+	LeafLeafL3LinkSpeed         *rawLogicalDevicePortSpeed `json:"leaf_leaf_l3_link_speed"`
+	LeafLeafLinkCount           int                        `json:"leaf_leaf_link_count"`
+	LeafLeafLinkPortChannelId   int                        `json:"leaf_leaf_link_port_channel_id"`
+	LeafLeafLinkSpeed           *rawLogicalDevicePortSpeed `json:"leaf_leaf_link_speed"`
+	LinkPerSpineCount           int                        `json:"link_per_spine_count"`
+	LinkPerSpineSpeed           *rawLogicalDevicePortSpeed `json:"link_per_spine_speed"`
+	LogicalDevice               ObjectId                   `json:"logical_device"`
+	MlagVlanId                  int                        `json:"mlag_vlan_id"`
+	RedundancyProtocol          leafRedundancyProtocol     `json:"redundancy_protocol,omitempty"`
+	Tags                        []TagLabel                 `json:"tags"`
 }
 
 func (o *rawRackElementLeafSwitch) polish(rack *rawRackType) (*RackElementLeafSwitch, error) {
@@ -583,14 +583,14 @@ type RackElementAccessSwitch struct {
 }
 
 type rawRackElementAccessSwitch struct {
-	InstanceCount         int                       `json:"instance_count"`
-	RedundancyProtocol    accessRedundancyProtocol  `json:"redundancy_protocol,omitempty"`
-	Links                 []RackLink                `json:"links"`
-	Label                 string                    `json:"label"`
-	LogicalDevice         ObjectId                  `json:"logical_device"`
-	AccessAccessLinkCount int                       `json:"access_access_link_count"`
-	AccessAccessLinkSpeed rawLogicalDevicePortSpeed `json:"access_access_link_speed"`
-	Tags                  []TagLabel                `json:"tags"`
+	InstanceCount         int                        `json:"instance_count"`
+	RedundancyProtocol    accessRedundancyProtocol   `json:"redundancy_protocol,omitempty"`
+	Links                 []RackLink                 `json:"links"`
+	Label                 string                     `json:"label"`
+	LogicalDevice         ObjectId                   `json:"logical_device"`
+	AccessAccessLinkCount int                        `json:"access_access_link_count"`
+	AccessAccessLinkSpeed *rawLogicalDevicePortSpeed `json:"access_access_link_speed"`
+	Tags                  []TagLabel                 `json:"tags"`
 }
 
 func (o *rawRackElementAccessSwitch) polish(rack *rawRackType) (*RackElementAccessSwitch, error) {

commit ddf864711bbba702180a1d58ef6952aff597ec97

fallout from 9892c4ede5e2e5139e05b40c36cfe9422d596f18, fix nil pointer exception

api_design_rack_types.go
@@ -510,16 +510,29 @@ func (o *rawRackElementLeafSwitch) polish(rack *rawRackType) (*RackElementLeafSw
 		tags = append(tags, *tag)
 	}
 
+	var leafLeafL3LinkSpeed LogicalDevicePortSpeed
+	if o.LeafLeafL3LinkSpeed != nil {
+		leafLeafL3LinkSpeed = o.LeafLeafL3LinkSpeed.parse()
+	}
+	var leafLeafLinkSpeed LogicalDevicePortSpeed
+	if o.LeafLeafLinkSpeed != nil {
+		leafLeafLinkSpeed = o.LeafLeafLinkSpeed.parse()
+	}
+	var linkPerSpineSpeed LogicalDevicePortSpeed
+	if o.LinkPerSpineSpeed != nil {
+		linkPerSpineSpeed = o.LinkPerSpineSpeed.parse()
+	}
+
 	result := &RackElementLeafSwitch{
 		Label:                       o.Label,
 		LeafLeafL3LinkCount:         o.LeafLeafL3LinkCount,
 		LeafLeafL3LinkPortChannelId: o.LeafLeafL3LinkPortChannelId,
-		LeafLeafL3LinkSpeed:         o.LeafLeafL3LinkSpeed.parse(),
+		LeafLeafL3LinkSpeed:         leafLeafL3LinkSpeed,
 		LeafLeafLinkCount:           o.LeafLeafLinkCount,
 		LeafLeafLinkPortChannelId:   o.LeafLeafLinkPortChannelId,
-		LeafLeafLinkSpeed:           o.LeafLeafLinkSpeed.parse(),
+		LeafLeafLinkSpeed:           leafLeafLinkSpeed,
 		LinkPerSpineCount:           o.LinkPerSpineCount,
-		LinkPerSpineSpeed:           o.LinkPerSpineSpeed.parse(),
+		LinkPerSpineSpeed:           linkPerSpineSpeed,
 		MlagVlanId:                  o.MlagVlanId,
 		RedundancyProtocol:          LeafRedundancyProtocol(rp),
 		Panels:                      pld.Panels,
@@ -619,6 +632,11 @@ func (o *rawRackElementAccessSwitch) polish(rack *rawRackType) (*RackElementAcce
 		tags = append(tags, *tag)
 	}
 
+	var accessAccessLinkSpeed LogicalDevicePortSpeed
+	if o.AccessAccessLinkSpeed != nil {
+		accessAccessLinkSpeed = o.AccessAccessLinkSpeed.parse()
+	}
+
 	return &RackElementAccessSwitch{
 		InstanceCount:         o.InstanceCount,
 		RedundancyProtocol:    AccessRedundancyProtocol(rp),
@@ -627,7 +645,7 @@ func (o *rawRackElementAccessSwitch) polish(rack *rawRackType) (*RackElementAcce
 		Panels:                pld.Panels,
 		DisplayName:           pld.DisplayName,
 		AccessAccessLinkCount: o.AccessAccessLinkCount,
-		AccessAccessLinkSpeed: o.AccessAccessLinkSpeed.parse(),
+		AccessAccessLinkSpeed: accessAccessLinkSpeed,
 		Tags:                  tags,
 	}, nil
 }

api_design_rack_types_test.go
@@ -9,6 +9,7 @@ import (
 )
 
 func TestListGetOneRackType(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
 	clients, err := getTestClients()
 	if err != nil {
 		t.Fatal(err)
@@ -20,10 +21,12 @@ func TestListGetOneRackType(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		_ = rtIds
 
-		log.Printf("testing getRackType() against %s %s (%s)", client.clientType, client.clientName, client.client.ApiVersion())
-		rt, err := client.client.GetRackType(context.TODO(), rtIds[0])
+		//id := rtIds[rand.Intn(len(rtIds))]
+		id := rtIds[0]
+
+		log.Printf("testing getRackType(%s) against %s %s (%s)", id, client.clientType, client.clientName, client.client.ApiVersion())
+		rt, err := client.client.GetRackType(context.TODO(), id)
 		if err != nil {
 			t.Fatal(err)
 		}