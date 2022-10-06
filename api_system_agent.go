e4b00290751f638ad0a9b9b0d59da4715676c04f
Chris Marget <cmarget@juniper.net>
2022-10-06 11:09:30 -0400

    add managed devices 'advanced features' capability

---
+++ api_system_agent.go
@@ -0,0 +1,30 @@
+package goapstra
+
+import (
+	"context"
+	"net/http"
+)
+
+const apiUrlSystemAgentManagerConfig = "/api/system-agent/manager-config"
+
+type SystemAgentManagerConfig struct {
+	SkipRevertToPristineOnUninstall bool `json:"skip_revert_to_pristine_on_uninstall"`
+	SkipPristineValidation          bool `json:"skip_pristine_validation"`
+}
+
+func (o *Client) getSystemAgentManagerConfig(ctx context.Context) (*SystemAgentManagerConfig, error) {
+	result := &SystemAgentManagerConfig{}
+	return result, o.talkToApstra(ctx, &talkToApstraIn{
+		method:      http.MethodGet,
+		urlStr:      apiUrlSystemAgentManagerConfig,
+		apiResponse: result,
+	})
+}
+
+func (o *Client) setSystemAgentManagerConfig(ctx context.Context, cfg *SystemAgentManagerConfig) error {
+	return o.talkToApstra(ctx, &talkToApstraIn{
+		method:   http.MethodPut,
+		urlStr:   apiUrlSystemAgentManagerConfig,
+		apiInput: cfg,
+	})
+}
---
+++ api_system_agent_test.go
@@ -0,0 +1,64 @@
+//go:build integration
+// +build integration
+
+package goapstra
+
+import (
+	"context"
+	"log"
+	"testing"
+)
+
+func TestGetSetSystemAgentManagerConfiguration(t *testing.T) {
+	clients, err := getTestClients()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for clientName, client := range clients {
+		client.client.Login(context.Background())
+		// initial fetch
+		log.Printf("testing getSystemAgentManagerConfig() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
+		mgrCfg, err := client.client.getSystemAgentManagerConfig(context.Background())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// new config with opposite values
+		testCfg := &SystemAgentManagerConfig{
+			SkipRevertToPristineOnUninstall: !mgrCfg.SkipRevertToPristineOnUninstall,
+			SkipPristineValidation:          !mgrCfg.SkipPristineValidation,
+		}
+
+		// set new config
+		log.Printf("testing setSystemAgentManagerConfig() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
+		err = client.client.setSystemAgentManagerConfig(context.Background(), testCfg)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// fetch new config
+		log.Printf("testing getSystemAgentManagerConfig() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
+		testCfgRetrieved, err := client.client.getSystemAgentManagerConfig(context.Background())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// validate field as expected
+		if testCfgRetrieved.SkipPristineValidation != testCfg.SkipPristineValidation {
+			t.Fatalf("error setting skip pristine validation")
+		}
+
+		// validate field as expected
+		if testCfgRetrieved.SkipRevertToPristineOnUninstall != testCfg.SkipRevertToPristineOnUninstall {
+			t.Fatalf("error setting skip revert to pristine on uninstall")
+		}
+
+		// reset to original configuration
+		log.Printf("testing setSystemAgentManagerConfig() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
+		err = client.client.setSystemAgentManagerConfig(context.Background(), mgrCfg)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
