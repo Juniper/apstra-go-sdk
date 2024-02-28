77d41553137a79df91f69f1551bbc3d3605d9728
bwJuniper <98770420+bwJuniper@users.noreply.github.com>
2024-02-28 07:52:02 -0800

    add different_values

--- apstra/api_blueprints_integration_test.go
+++ apstra/api_blueprints_integration_test.go
@@ -393,6 +393,41 @@ func TestCreateDeleteBlueprintNew(t *testing.T) {
 				},
 			},
 		},
+		"different_values": {
+			// 4.2.1 put all the goodies in there from fabric_settings
+			versionConstraints: version.MustConstraints(version.NewConstraint(">" + apstra412)),
+			req: CreateBlueprintFromTemplateRequest{
+				RefDesign:  RefDesignTwoStageL3Clos,
+				Label:      randString(5, "hex"),
+				TemplateId: "L2_Virtual_EVPN",
+				FabricSettings: &FabricSettings{
+					JunosEvpnDuplicateMacRecoveryTime:     toPtr(uint16(14)),
+					MaxExternalRoutes:                     toPtr(uint32(233832)),
+					EsiMacMsb:                             toPtr(uint8(50)),
+					JunosGracefulRestart:                  &FeatureSwitchEnumEnabled,
+					OptimiseSzFootprint:                   &FeatureSwitchEnumDisabled,
+					JunosEvpnRoutingInstanceVlanAware:     &FeatureSwitchEnumDisabled,
+					EvpnGenerateType5HostRoutes:           &FeatureSwitchEnumEnabled,
+					MaxFabricRoutes:                       toPtr(uint32(82231)),
+					MaxMlagRoutes:                         toPtr(uint32(74112)),
+					JunosExOverlayEcmp:                    &FeatureSwitchEnumDisabled,
+					DefaultSviL3Mtu:                       toPtr(uint16(9070)),
+					JunosEvpnMaxNexthopAndInterfaceNumber: &FeatureSwitchEnumDisabled,
+					FabricL3Mtu:                           toPtr(uint16(9172)),
+					Ipv6Enabled:                           toPtr(false), // do not enable because it's a one-way trip
+					ExternalRouterMtu:                     toPtr(uint16(9080)),
+					MaxEvpnRoutes:                         toPtr(uint32(91342)),
+					AntiAffinityPolicy: &AntiAffinityPolicy{
+						Algorithm:                AlgorithmHeuristic,
+						MaxLinksPerPort:          4,
+						MaxLinksPerSlot:          4,
+						MaxPerSystemLinksPerPort: 4,
+						MaxPerSystemLinksPerSlot: 4,
+						Mode:                     AntiAffinityModeEnabledLoose,
+					},
+				},
+			},
+		},
 	}
 
 	fetchFabricAddressingScheme := func(t testing.TB, client *TwoStageL3ClosClient) (AddressingScheme, AddressingScheme) {
