7497327171b2d1bb826ca4ab866825ffd70f0741
Chris Marget <cmarget@juniper.net>
2024-06-28 08:56:33 -0400

    code review

--- apstra/api_telemetry_service_registry.go
+++ apstra/api_telemetry_service_registry.go
@@ -9,82 +9,93 @@ import (
 
 const (
 	apiUrlTelemetryServiceRegistry            = "/api/telemetry-service-registry"
-	apiUrlTelemetryServiceRegistryPrefix      = apiUrlTelemetryServiceRegistry + apiUrlPathDelim
-	apiUrlTelemetryServiceRegistryEntryByName = apiUrlTelemetryServiceRegistryPrefix + "%s"
+	apiUrlTelemetryServiceRegistryEntryByName = apiUrlTelemetryServiceRegistry + apiUrlPathDelim + "%s"
 )
 
-type rawTelemetryServiceRegistryEntry struct {
-	ServiceName       string          `json:"service_name"`
-	ApplicationSchema json.RawMessage `json:"application_schema"`
-	StorageSchemaPath string          `json:"storage_schema_path"`
-	Builtin           bool            `json:"builtin"`
-	Description       string          `json:"description"`
-	Version           string          `json:"version"`
-}
+var (
+	_ json.Marshaler   = (*TelemetryServiceRegistryEntry)(nil)
+	_ json.Unmarshaler = (*TelemetryServiceRegistryEntry)(nil)
+)
 
 type TelemetryServiceRegistryEntry struct {
 	ServiceName       string
-	StorageSchemaPath StorageSchemaPath
 	ApplicationSchema json.RawMessage
+	StorageSchemaPath StorageSchemaPath
 	Builtin           bool
 	Description       string
 	Version           string
 }
 
 func (o *TelemetryServiceRegistryEntry) UnmarshalJSON(data []byte) error {
-	var sspath StorageSchemaPath
-	var r rawTelemetryServiceRegistryEntry
-	err := json.Unmarshal(data, &r)
+	var raw struct {
+		ServiceName       string          `json:"service_name"`
+		ApplicationSchema json.RawMessage `json:"application_schema"`
+		StorageSchemaPath string          `json:"storage_schema_path"`
+		Builtin           bool            `json:"builtin"`
+		Description       string          `json:"description"`
+		Version           string          `json:"version"`
+	}
+
+	err := json.Unmarshal(data, &raw)
 	if err != nil {
 		return err
 	}
-	err = sspath.FromString(r.StorageSchemaPath)
+
+	o.ServiceName = raw.ServiceName
+	o.ApplicationSchema = raw.ApplicationSchema
+	o.Builtin = raw.Builtin
+	o.Description = raw.Description
+	o.Version = raw.Version
+	err = o.StorageSchemaPath.FromString(raw.StorageSchemaPath)
 	if err != nil {
 		return err
 	}
-	*o = TelemetryServiceRegistryEntry{
-		ServiceName:       r.ServiceName,
-		StorageSchemaPath: sspath,
-		ApplicationSchema: r.ApplicationSchema,
-		Builtin:           r.Builtin,
-		Description:       r.Description,
-		Version:           r.Version,
-	}
+
 	return nil
 }
 
 func (o *TelemetryServiceRegistryEntry) MarshalJSON() ([]byte, error) {
+	var raw struct {
+		ServiceName       string          `json:"service_name"`
+		ApplicationSchema json.RawMessage `json:"application_schema"`
+		StorageSchemaPath string          `json:"storage_schema_path"`
+		Builtin           bool            `json:"builtin"`
+		Description       string          `json:"description"`
+		Version           string          `json:"version"`
+	}
 
-	return json.Marshal(rawTelemetryServiceRegistryEntry{
-		ServiceName:       o.ServiceName,
-		StorageSchemaPath: o.StorageSchemaPath.String(),
-		Builtin:           o.Builtin,
-		Description:       o.Description,
-		Version:           o.Version,
-		ApplicationSchema: o.ApplicationSchema,
-	})
+	raw.ServiceName = o.ServiceName
+	raw.StorageSchemaPath = o.StorageSchemaPath.String()
+	raw.Builtin = o.Builtin
+	raw.Description = o.Description
+	raw.Version = o.Version
+	raw.ApplicationSchema = o.ApplicationSchema
+
+	return json.Marshal(raw)
 }
 
 // GetAllTelemetryServiceRegistryEntries gets all the Telemetry Service Registry Entries
 func (o *Client) GetAllTelemetryServiceRegistryEntries(ctx context.Context) ([]TelemetryServiceRegistryEntry, error) {
-	response := &struct {
+	var response struct {
 		Items []TelemetryServiceRegistryEntry `json:"items"`
-	}{}
+	}
 
 	err := o.talkToApstra(ctx, &talkToApstraIn{
 		method:      http.MethodGet,
 		urlStr:      fmt.Sprintf(apiUrlTelemetryServiceRegistry),
-		apiResponse: response,
+		apiResponse: &response,
 	})
 	if err != nil {
 		return nil, convertTtaeToAceWherePossible(err)
 	}
+
 	return response.Items, nil
 }
 
 // GetTelemetryServiceRegistryEntry gets all the Telemetry Service Registry Entries
 func (o *Client) GetTelemetryServiceRegistryEntry(ctx context.Context, name string) (*TelemetryServiceRegistryEntry, error) {
-	response := &TelemetryServiceRegistryEntry{}
+	var response TelemetryServiceRegistryEntry
+
 	err := o.talkToApstra(ctx, &talkToApstraIn{
 		method:      http.MethodGet,
 		urlStr:      fmt.Sprintf(apiUrlTelemetryServiceRegistryEntryByName, name),
@@ -93,49 +104,57 @@ func (o *Client) GetTelemetryServiceRegistryEntry(ctx context.Context, name stri
 	if err != nil {
 		return nil, convertTtaeToAceWherePossible(err)
 	}
-	return response, nil
+
+	return &response, nil
 }
 
 // CreateTelemetryServiceRegistryEntry creates a telemetry service registry entry
-func (o *Client) CreateTelemetryServiceRegistryEntry(ctx context.Context, r TelemetryServiceRegistryEntry) (string, error) {
-	response := &struct {
+func (o *Client) CreateTelemetryServiceRegistryEntry(ctx context.Context, in TelemetryServiceRegistryEntry) (string, error) {
+	var response struct {
 		Name string `json:"service_name"`
-	}{}
+	}
 
 	err := o.talkToApstra(ctx, &talkToApstraIn{
 		method:      http.MethodPost,
 		urlStr:      fmt.Sprintf(apiUrlTelemetryServiceRegistry),
-		apiInput:    &r,
+		apiInput:    &in,
 		apiResponse: &response,
 	})
 	if err != nil {
 		return "", err
 	}
+
 	return response.Name, nil
 }
 
 // UpdateTelemetryServiceRegistryEntry updates a telemetry service registry entry
-func (o *Client) UpdateTelemetryServiceRegistryEntry(ctx context.Context, name string, r *TelemetryServiceRegistryEntry) error {
-	response := &struct {
+func (o *Client) UpdateTelemetryServiceRegistryEntry(ctx context.Context, name string, in *TelemetryServiceRegistryEntry) error {
+	var response struct {
 		Name string `json:"service_name"`
-	}{}
+	}
+
 	err := o.talkToApstra(ctx, &talkToApstraIn{
 		method:      http.MethodPut,
 		urlStr:      fmt.Sprintf(apiUrlTelemetryServiceRegistryEntryByName, name),
-		apiInput:    &r,
+		apiInput:    in,
 		apiResponse: &response,
 	})
-
 	if err != nil {
 		return convertTtaeToAceWherePossible(err)
 	}
+
 	return nil
 }
 
 // DeleteTelemetryServiceRegistryEntry deletes a telemetry service registry entry
 func (o *Client) DeleteTelemetryServiceRegistryEntry(ctx context.Context, name string) error {
-	return convertTtaeToAceWherePossible(o.talkToApstra(ctx, &talkToApstraIn{
+	err := o.talkToApstra(ctx, &talkToApstraIn{
 		method: http.MethodDelete,
 		urlStr: fmt.Sprintf(apiUrlTelemetryServiceRegistryEntryByName, name),
-	}))
+	})
+	if err != nil {
+		return convertTtaeToAceWherePossible(err)
+	}
+
+	return nil
 }
