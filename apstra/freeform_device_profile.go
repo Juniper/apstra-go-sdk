8a2dd4c340e8e8ae4e0617f5eb21cc6bcc9f957f
bwjuniper <bwester@juniper.net>
2024-08-19 11:04:34 -0700

    initial device_profile work, needs review

--- apstra/freeform_device_profile.go
+++ apstra/freeform_device_profile.go
@@ -37,6 +37,74 @@ func (o *FreeformClient) ImportDeviceProfile(ctx context.Context, id ObjectId) (
 	return response.Ids[0], nil
 }
 
+func (o *FreeformClient) GetDeviceProfilesByName(ctx context.Context, desired string) ([]DeviceProfile, error) {
+	deviceProfiles, err := o.getAllDeviceProfiles(ctx)
+	if err != nil {
+		return nil, err
+	}
+	var result []DeviceProfile
+	for _, deviceProfile := range deviceProfiles {
+		if deviceProfile.Label == desired {
+			result = append(result, *deviceProfile.polish())
+		}
+	}
+	return result, nil
+}
+
+func (o *FreeformClient) GetDeviceProfilesById(ctx context.Context, desired ObjectId) (*DeviceProfile, error) {
+	deviceProfiles, err := o.getAllDeviceProfiles(ctx)
+	if err != nil {
+		return nil, err
+	}
+	var result DeviceProfile
+	for _, deviceProfile := range deviceProfiles {
+		if deviceProfile.Id == desired {
+			result = *deviceProfile.polish()
+		}
+	}
+	// todo I think we need to figure out how to return this as a DeviceProfile type instead of a []rawDeviceProfile... is that the Polish[]?
+	// todo please have Chris review.
+	return &result, nil
+}
+
+func (o *FreeformClient) getAllDeviceProfiles(ctx context.Context) ([]rawDeviceProfile, error) {
+	response := &struct {
+		Items []rawDeviceProfile
+	}{}
+
+	err := o.client.talkToApstra(ctx, &talkToApstraIn{
+		method:      http.MethodGet,
+		urlStr:      fmt.Sprintf(apiUrlFfDp, o.blueprintId),
+		apiResponse: response,
+	})
+	return response.Items, convertTtaeToAceWherePossible(err)
+}
+
+func (o *FreeformClient) UpdateDeviceProfile(ctx context.Context, id ObjectId) error {
+	var response struct {
+		Ids []ObjectId `json:"ids"`
+	}
+
+	err := o.client.talkToApstra(ctx, &talkToApstraIn{
+		method: http.MethodPost,
+		urlStr: fmt.Sprintf(apiUrlFfDpImport, o.blueprintId),
+		apiInput: struct {
+			DeviceProfileIds []ObjectId `json:"device_profile_ids"`
+		}{
+			DeviceProfileIds: []ObjectId{id},
+		},
+		apiResponse: &response,
+	})
+	if err != nil {
+		return convertTtaeToAceWherePossible(err)
+	}
+	if len(response.Ids) != 1 {
+		return fmt.Errorf("expected one ObjectId got %d", len(response.Ids))
+	}
+
+	return nil
+}
+
 func (o *FreeformClient) DeleteDeviceProfile(ctx context.Context, id ObjectId) error {
 	err := o.client.talkToApstra(ctx, &talkToApstraIn{
 		method: http.MethodDelete,
