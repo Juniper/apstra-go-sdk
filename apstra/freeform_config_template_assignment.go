fc26de1dc3263600ece8962bbac826e44da96a95
bwjuniper <bwester@juniper.net>
2024-08-30 11:56:30 -0700

    add tests and fix methods

--- apstra/freeform_config_template_assignment.go
+++ apstra/freeform_config_template_assignment.go
@@ -10,9 +10,10 @@ const (
 	apiUrlConfigTemplateAssignments = apiUrlBlueprintById + apiUrlPathDelim + "config-templates-assignments"
 )
 
-func (o *FreeformClient) ListConfigTemplateAssignments(ctx context.Context) (map[ObjectId]ObjectId, error) {
+// ListConfigTemplateAssignments returns map of [ObjectId]*ObjectId where keys are system IDs and values are Config Template IDs
+func (o *FreeformClient) ListConfigTemplateAssignments(ctx context.Context) (map[ObjectId]*ObjectId, error) {
 	var response struct {
-		Assignments map[ObjectId]ObjectId `json:"assignments"`
+		Assignments map[ObjectId]*ObjectId `json:"assignments"`
 	}
 
 	err := o.client.talkToApstra(ctx, &talkToApstraIn{
@@ -35,30 +36,77 @@ func (o *FreeformClient) GetConfigTemplateAssignments(ctx context.Context, in Ob
 
 	var result []ObjectId
 	for k, v := range assignments {
-		if v == in {
+		if *v == in {
 			result = append(result, k)
 		}
 	}
 	return result, nil
 }
 
-func (o *FreeformClient) UpdateConfigTemplateAssignments(ctx context.Context, ctId ObjectId, sysIds []ObjectId) error {
-	var apiInput struct {
-		Assignments map[ObjectId]ObjectId `json:"assignments"`
-	}
-
-	apiInput.Assignments = make(map[ObjectId]ObjectId, len(sysIds))
-	for _, sysId := range sysIds {
-		apiInput.Assignments[sysId] = ctId
-	}
-
+// UpdateConfigTemplateAssignments returns map [ObjectId]*ObjectId where keys are system IDs and values are Config Template IDs.
+// A nil value will clear the assignment of the Config Template to the System.
+func (o *FreeformClient) UpdateConfigTemplateAssignments(ctx context.Context, assignments map[ObjectId]*ObjectId) error {
 	err := o.client.talkToApstra(ctx, &talkToApstraIn{
-		method:   http.MethodPatch,
-		urlStr:   fmt.Sprintf(apiUrlConfigTemplateAssignments, o.blueprintId),
-		apiInput: apiInput,
+		method: http.MethodPatch,
+		urlStr: fmt.Sprintf(apiUrlConfigTemplateAssignments, o.blueprintId),
+		apiInput: struct {
+			Assignments map[ObjectId]*ObjectId `json:"assignments"`
+		}{
+			Assignments: assignments,
+		},
 	})
 	if err != nil {
 		return convertTtaeToAceWherePossible(err)
+	}
+
+	return nil
+}
+
+func (o *FreeformClient) UpdateConfigTemplateAssignmentsByTemplate(ctx context.Context, ctID ObjectId, sysIDs []ObjectId) error {
+	// set a lock and defer the unlock until return
+	o.client.lock("configTemplateAssignments")
+	defer o.client.unlock("configTemplateAssignments")
+
+	// read the current set of CT assignments from the api and load it into current.
+	current, err := o.GetConfigTemplateAssignments(ctx, ctID)
+	if err != nil {
+		return err
+	}
+
+	// turn current into a map [ObjectID]struct{}
+	currentMap := make(map[ObjectId]bool)
+	for _, sysID := range current {
+		currentMap[sysID] = true
+	}
+
+	request := make(map[ObjectId]*ObjectId)
+
+	// desired map
+	desiredMap := make(map[ObjectId]bool)
+	for _, sysID := range sysIDs {
+		desiredMap[sysID] = true
+
+		if !currentMap[sysID] {
+			// lookup failed, add
+			request[sysID] = &ctID
+		}
+	}
+
+	// fill the request map with the ids to be deleted
+	for _, sysID := range current {
+		if !desiredMap[sysID] {
+			request[sysID] = nil
+		}
+	}
+
+	if len(request) == 0 {
+		return nil
+	}
+
+	// Update the assignments with the newly formed request.
+	err = o.UpdateConfigTemplateAssignments(ctx, request)
+	if err != nil {
+		return err
 	}
 
 	return nil
---
+++ apstra/freeform_config_template_assignment_integration_test.go
@@ -0,0 +1,133 @@
+//go:build integration
+// +build integration
+
+package apstra
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_ConfigTemplate_Assignment(t *testing.T) {
+	ctx := context.Background()
+	clients, err := getTestClients(ctx, t)
+	require.NoError(t, err)
+
+	intSysCount := 4
+	ctCount := 4
+
+	type testStep struct {
+		ctId   ObjectId
+		sysIds []ObjectId
+	}
+	type testCase struct {
+		steps []testStep
+	}
+
+	for clientName, client := range clients {
+		t.Run(clientName, func(t *testing.T) {
+			t.Parallel()
+
+			ffc, intSysIds, _ := testFFBlueprintB(ctx, t, client.client, intSysCount, 0)
+
+			ctIds := make([]ObjectId, ctCount)
+			for i := range ctIds {
+				id, err := ffc.CreateConfigTemplate(ctx, &ConfigTemplateData{
+					Label: randString(6, "hex") + ".jinja",
+					Text:  randString(30, "hex"),
+				})
+				require.NoError(t, err)
+				ctIds[i] = id
+			}
+
+			testCases := map[string]testCase{
+				"single_sysID": testCase{
+					steps: []testStep{
+						{
+							ctId:   ctIds[0],
+							sysIds: []ObjectId{intSysIds[0]},
+						},
+						{
+							ctId:   ctIds[0],
+							sysIds: []ObjectId{intSysIds[1]},
+						},
+						{
+							ctId:   ctIds[0],
+							sysIds: []ObjectId{intSysIds[2]},
+						},
+						{
+							ctId:   ctIds[0],
+							sysIds: []ObjectId{intSysIds[0]},
+						},
+					},
+				},
+				"set_clear_set_single_sysID": testCase{
+					steps: []testStep{
+						{
+							ctId:   ctIds[1],
+							sysIds: []ObjectId{intSysIds[0]},
+						},
+						{
+							ctId:   ctIds[1],
+							sysIds: []ObjectId{},
+						},
+						{
+							ctId:   ctIds[1],
+							sysIds: []ObjectId{intSysIds[0]},
+						},
+					},
+				},
+				"clear_set_clear_single_sysID": testCase{
+					steps: []testStep{
+						{
+							ctId:   ctIds[2],
+							sysIds: []ObjectId{},
+						},
+						{
+							ctId:   ctIds[2],
+							sysIds: []ObjectId{intSysIds[0]},
+						},
+						{
+							ctId:   ctIds[2],
+							sysIds: []ObjectId{},
+						},
+					},
+				},
+				"exercise_multiple_systems": testCase{
+					steps: []testStep{
+						{
+							ctId:   ctIds[3],
+							sysIds: []ObjectId{},
+						},
+						{
+							ctId:   ctIds[3],
+							sysIds: []ObjectId{intSysIds[0], intSysIds[1]},
+						},
+						{
+							ctId:   ctIds[2],
+							sysIds: []ObjectId{intSysIds[1], intSysIds[2]},
+						},
+						{
+							ctId:   ctIds[2],
+							sysIds: []ObjectId{intSysIds[0], intSysIds[3]},
+						},
+					},
+				},
+			}
+
+			for tName, tCase := range testCases {
+				tName, tCase := tName, tCase
+				t.Run(tName, func(t *testing.T) {
+					for _, step := range tCase.steps {
+						require.NoError(t, ffc.UpdateConfigTemplateAssignmentsByTemplate(ctx, step.ctId, step.sysIds))
+						assigned, err := ffc.GetConfigTemplateAssignments(ctx, step.ctId)
+						require.NoError(t, err)
+						compareSlicesAsSets(t, step.sysIds, assigned, "assignment mismatch after update")
+					}
+				})
+			}
+		})
+	}
+}
