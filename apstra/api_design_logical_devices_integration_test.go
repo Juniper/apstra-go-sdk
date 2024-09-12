9b2545e9e3e4264e51f1889a79bce668c4f29f12
Chris Marget <cmarget@juniper.net>
2024-09-12 13:22:12 -0400

    update logical device tests

---
+++ apstra/api_design_logical_devices_integration_test.go
@@ -0,0 +1,159 @@
+//go:build integration
+
+package apstra
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"log"
+	"testing"
+)
+
+func TestListAndGetAllLogicalDevices(t *testing.T) {
+	clients, err := getTestClients(context.Background(), t)
+	require.NoError(t, err)
+
+	for clientName, client := range clients {
+		clientName, client := clientName, client
+		t.Run(fmt.Sprintf("%s_%s", client.client.apiVersion, clientName), func(t *testing.T) {
+			t.Parallel()
+
+			log.Printf("testing listLogicalDeviceIds() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
+			ids, err := client.client.listLogicalDeviceIds(context.TODO())
+			require.NoError(t, err)
+			require.NotEqual(t, len(ids), 0)
+
+			for _, i := range samples(len(ids)) {
+				id := ids[i]
+				t.Run(fmt.Sprintf("GET_%s", id), func(t *testing.T) {
+					t.Parallel()
+
+					ld, err := client.client.GetLogicalDevice(context.TODO(), id)
+					require.NoError(t, err)
+					require.Equal(t, id, ld.Id)
+				})
+			}
+		})
+	}
+}
+
+func TestCreateGetUpdateDeleteLogicalDevice(t *testing.T) {
+	ctx := context.Background()
+
+	clients, err := getTestClients(ctx, t)
+	require.NoError(t, err)
+
+	indexingTypes := []string{
+		PortIndexingVerticalFirst,
+		PortIndexingHorizontalFirst,
+	}
+
+	for clientName, client := range clients {
+		clientName, client := clientName, client
+		t.Run(fmt.Sprintf("%s_%s", client.client.apiVersion, clientName), func(t *testing.T) {
+			t.Parallel()
+
+			deviceConfigs := make([]LogicalDeviceData, len(indexingTypes))
+			for i, indexing := range indexingTypes {
+				deviceConfigs[i] = LogicalDeviceData{
+					DisplayName: randString(6, "hex"),
+					Panels: []LogicalDevicePanel{
+						{
+							PanelLayout: LogicalDevicePanelLayout{
+								RowCount:    2,
+								ColumnCount: 2,
+							},
+							PortIndexing: LogicalDevicePortIndexing{
+								Order:      indexing,
+								StartIndex: 0,
+								Schema:     "absolute",
+							},
+							PortGroups: []LogicalDevicePortGroup{
+								{
+									Count: 4,
+									Speed: "10G",
+									Roles: LogicalDevicePortRoleUnused,
+								},
+							},
+						},
+					},
+				}
+			}
+
+			ids := make([]ObjectId, len(deviceConfigs))
+			for i, devCfg := range deviceConfigs {
+				log.Printf("testing createLogicalDevice() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
+				ids[i], err = client.client.createLogicalDevice(ctx, devCfg.raw())
+				require.NoError(t, err)
+
+				log.Printf("testing GetLogicalDevice() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
+				d, err := client.client.GetLogicalDevice(ctx, ids[i])
+				require.NoError(t, err)
+
+				log.Println(d.Id)
+				devCfg.Panels[0].PortIndexing.StartIndex = 1
+				log.Printf("testing updateLogicalDevice() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
+				require.NoError(t, client.client.updateLogicalDevice(ctx, d.Id, devCfg.raw()))
+
+				if i > 0 {
+					log.Printf("testing GetLogicalDevice() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
+					previous, err := client.client.GetLogicalDevice(ctx, ids[i-1])
+					require.NoError(t, err)
+
+					previous.Data.DisplayName = d.Data.DisplayName
+
+					log.Printf("testing updateLogicalDevice() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
+					require.NoError(t, client.client.updateLogicalDevice(ctx, ids[i], previous.Data.raw()))
+				}
+
+				log.Printf("testing GetLogicalDevice() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
+				_, err = client.client.GetLogicalDevice(ctx, ids[i])
+				require.NoError(t, err)
+
+			}
+
+			for _, id := range ids {
+				t.Run(fmt.Sprintf("DELETE_logical_device_%s", id), func(t *testing.T) {
+					t.Parallel()
+					require.NoError(t, client.client.deleteLogicalDevice(ctx, id))
+				})
+
+			}
+		})
+	}
+}
+
+func TestGetLogicalDeviceByName(t *testing.T) {
+	ctx := context.Background()
+
+	clients, err := getTestClients(ctx, t)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for clientName, client := range clients {
+		clientName, client := clientName, client
+		t.Run(fmt.Sprintf("%s_%s", client.client.apiVersion, clientName), func(t *testing.T) {
+			t.Parallel()
+
+			log.Printf("testing deleteLogicalDevice() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
+			logicalDevices, err := client.client.getAllLogicalDevices(context.TODO())
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			for _, i := range samples(len(logicalDevices)) {
+				test := logicalDevices[i]
+				t.Run(fmt.Sprintf("GET_LD_%s", test.Data.DisplayName), func(t *testing.T) {
+					t.Parallel()
+
+					logicalDevice, err := client.client.GetLogicalDeviceByName(context.TODO(), test.Data.DisplayName)
+					require.NoError(t, err)
+					require.Equal(t, test.Id, logicalDevice.Id)
+					require.Equal(t, test.Data.DisplayName, logicalDevice.Data.DisplayName)
+				})
+			}
+		})
+	}
+}
---
+++ apstra/api_design_logical_devices_unit_test.go
@@ -0,0 +1,82 @@
+package apstra
+
+import (
+	"fmt"
+	"testing"
+)
+
+var testSpeedStrings = [][]string{
+	{"10000000", "10M"},
+	{"10M", "10M"},
+	{"10Mbps", "10M"},
+	{"10Mb/s", "10M"},
+	{"100000000", "100M"},
+	{"100M", "100M"},
+	{"100Mbps", "100M"},
+	{"100Mb/s", "100M"},
+	{"1000000000", "1G"},
+	{"1000M", "1G"},
+	{"1000Mbps", "1G"},
+	{"1000Mb/s", "1G"},
+	{"1000000000", "1G"},
+	{"10G", "10G"},
+	{"10Gbps", "10G"},
+	{"10Gb/s", "10G"},
+	{"10000000000", "10G"},
+	{"25G", "25G"},
+	{"25Gbps", "25G"},
+	{"25Gb/s", "25G"},
+	{"25000000000", "25G"},
+	{"40G", "40G"},
+	{"40Gbps", "40G"},
+	{"40Gb/s", "40G"},
+	{"40000000000", "40G"},
+	{"50G", "50G"},
+	{"50Gbps", "50G"},
+	{"50Gb/s", "50G"},
+	{"50000000000", "50G"},
+	{"100G", "100G"},
+	{"100Gbps", "100G"},
+	{"100Gb/s", "100G"},
+	{"100000000000", "100G"},
+	{"200G", "200G"},
+	{"200Gbps", "200G"},
+	{"200Gb/s", "200G"},
+	{"200000000000", "200G"},
+	{"400G", "400G"},
+	{"400Gbps", "400G"},
+	{"400Gb/s", "400G"},
+	{"400000000000", "400G"},
+}
+
+func TestParseLogicalDeviceSpeed(t *testing.T) {
+	for _, test := range testSpeedStrings {
+		r := LogicalDevicePortSpeed(test[0]).raw()
+		s1 := fmt.Sprintf("%d%s", r.Value, r.Unit)
+		s2 := string(r.parse())
+		if s1 != s2 {
+			t.Fatalf("conversion problem: %s %s %s %s", test[0], test[1], s1, s2)
+		}
+	}
+}
+
+func TestLogicalDevicePortSpeed_IsEqual(t *testing.T) {
+	for _, s := range testSpeedStrings {
+		if !LogicalDevicePortSpeed(s[0]).IsEqual(LogicalDevicePortSpeed(s[1])) {
+			t.Fatalf("speeds not equal %s %s", s[0], s[1])
+		}
+	}
+}
+
+func TestLogicalDevicePortRoleFlagsFromStrings(t *testing.T) {
+	data := []string{"spine", "leaf"}
+	expected := LogicalDevicePortRoleSpine | LogicalDevicePortRoleLeaf
+	var result LogicalDevicePortRoleFlags
+	err := result.FromStrings(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result != expected {
+		t.Fatalf("expected %d, got %d", expected, result)
+	}
+}
