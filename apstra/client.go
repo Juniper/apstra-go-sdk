5d84624730424124c2dbada279ef4b67c0d2a4b4
Chris Marget <cmarget@juniper.net>
2025-03-01 18:58:24 -0500

    make EnvAosOpsEdgeId public

--- apstra/client.go
+++ apstra/client.go
@@ -285,9 +285,9 @@ func (o ClientCfg) validate() error {
 
 // NewClient creates a Client object
 func (o ClientCfg) NewClient(ctx context.Context) (*Client, error) {
-	if proxyId, ok := os.LookupEnv(envAosOpsEdgeId); ok {
+	if proxyId, ok := os.LookupEnv(EnvAosOpsEdgeId); ok {
 		if proxyId == "" {
-			return nil, fmt.Errorf("environment variable %s must not be empty if set", envAosOpsEdgeId)
+			return nil, fmt.Errorf("environment variable %s must not be empty if set", EnvAosOpsEdgeId)
 		}
 		o.apiOpsDcId = &proxyId
 	}
