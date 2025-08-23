79d560deee5b7915aa2369641ce719fdce91e365
Chris Marget <cmarget@juniper.net>
2025-08-23 19:52:42 -0400

    convert `TestInterfaceSettingParam()` to table-driven

--- apstra/api_design_interface_maps_test.go
+++ apstra/api_design_interface_maps_test.go
@@ -5,33 +5,49 @@
 package apstra_test
 
 import (
+	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/Juniper/apstra-go-sdk/apstra"
 )
 
 func TestInterfaceSettingParam(t *testing.T) {
-	expected := `{\"global\":{\"breakout\":false,\"fpc\":0,\"pic\":0,\"port\":0,\"speed\":\"100g\"},\"interface\":{\"speed\":\"\"}}`
-	test := apstra.InterfaceSettingParam{
-		Global: struct {
-			Breakout bool   `json:"breakout"`
-			Fpc      int    `json:"fpc"`
-			Pic      int    `json:"pic"`
-			Port     int    `json:"port"`
-			Speed    string `json:"speed"`
-		}{
-			Breakout: false,
-			Fpc:      0,
-			Pic:      0,
-			Port:     0,
-			Speed:    "100g",
-		},
-		Interface: struct {
-			Speed string `json:"speed"`
-		}{},
+
+	type testCase struct {
+		expected string
+		val      apstra.InterfaceSettingParam
 	}
-	result := test.String()
-	if result != expected {
-		t.Fatalf("expected '%s', got '%s'", expected, result)
+
+	testCases := map[string]testCase{
+		"a": {
+			expected: `{\"global\":{\"breakout\":false,\"fpc\":0,\"pic\":0,\"port\":0,\"speed\":\"100g\"},\"interface\":{\"speed\":\"\"}}`,
+			val: apstra.InterfaceSettingParam{
+				Global: struct {
+					Breakout bool   `json:"breakout"`
+					Fpc      int    `json:"fpc"`
+					Pic      int    `json:"pic"`
+					Port     int    `json:"port"`
+					Speed    string `json:"speed"`
+				}{
+					Breakout: false,
+					Fpc:      0,
+					Pic:      0,
+					Port:     0,
+					Speed:    "100g",
+				},
+				Interface: struct {
+					Speed string `json:"speed"`
+				}{},
+			},
+		},
+	}
+
+	for tName, tCase := range testCases {
+		t.Run(tName, func(t *testing.T) {
+			t.Parallel()
+
+			result := tCase.val.String()
+			require.Equal(t, tCase.expected, result)
+		})
 	}
 }
