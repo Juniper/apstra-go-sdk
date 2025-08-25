291cbea53a06c11e79495cc117e31683fb993bc5
Chris Marget <cmarget@juniper.net>
2025-08-25 16:05:22 -0400

    migrate rack type tests to use new test packages

---
+++ apstra/api_design_rack_types_integration_test.go
@@ -0,0 +1,153 @@
+// Copyright (c) Juniper Networks, Inc., 2025-2025.
+// All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build integration
+
+package apstra_test
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	"github.com/Juniper/apstra-go-sdk/apstra"
+	"github.com/Juniper/apstra-go-sdk/apstra/enum"
+	testutils "github.com/Juniper/apstra-go-sdk/internal/test_utils"
+	"github.com/Juniper/apstra-go-sdk/internal/test_utils/compare"
+	testclient "github.com/Juniper/apstra-go-sdk/internal/test_utils/test_client"
+	"github.com/stretchr/testify/require"
+)
+
+func TestListGetOneRackType(t *testing.T) {
+	ctx := testutils.WrapCtxWithTestId(t, context.Background())
+	clients := testclient.GetTestClients(t, ctx)
+
+	for _, client := range clients {
+		t.Run(client.Name(), func(t *testing.T) {
+			t.Parallel()
+			ctx = testutils.WrapCtxWithTestId(t, ctx)
+
+			rtIds, err := client.Client.ListRackTypeIds(ctx)
+			require.NoError(t, err)
+			require.NotZero(t, len(rtIds))
+
+			id := rtIds[0]
+
+			rt, err := client.Client.GetRackType(ctx, id)
+			require.NoError(t, err)
+
+			log.Println(rt.Id)
+		})
+	}
+}
+
+func TestListGetAllGetRackType(t *testing.T) {
+	ctx := testutils.WrapCtxWithTestId(t, context.Background())
+	clients := testclient.GetTestClients(t, ctx)
+
+	for _, client := range clients {
+		t.Run(client.Name(), func(t *testing.T) {
+			t.Parallel()
+			ctx = testutils.WrapCtxWithTestId(t, ctx)
+
+			rackTypeIds, err := client.Client.ListRackTypeIds(ctx)
+			require.NoError(t, err)
+			require.NotZero(t, len(rackTypeIds))
+
+			rackTypes, err := client.Client.GetAllRackTypes(ctx)
+			require.NoError(t, err)
+			require.NotZero(t, len(rackTypes))
+
+			require.Equal(t, len(rackTypeIds), len(rackTypes))
+
+			for _, i := range testutils.SampleIndexes(t, len(rackTypeIds)) {
+				id := rackTypeIds[i]
+
+				rt, err := client.Client.GetRackType(ctx, id)
+				require.NoError(t, err)
+
+				require.Contains(t, rackTypeIds, rt.Id)
+
+				log.Println(rt.Id)
+			}
+		})
+	}
+}
+
+func TestCreateGetRackDeleteRackType(t *testing.T) {
+	ctx := testutils.WrapCtxWithTestId(t, context.Background())
+	clients := testclient.GetTestClients(t, ctx)
+
+	leafLabel := "ll-" + testutils.RandString(10, "hex")
+
+	testCases := map[string]apstra.RackTypeRequest{
+		"leaf_only_no_tags": {
+			DisplayName:              "rdn " + testutils.RandString(5, "hex"),
+			FabricConnectivityDesign: enum.FabricConnectivityDesignL3Clos,
+			LeafSwitches: []apstra.RackElementLeafSwitchRequest{
+				{
+					Label:             leafLabel,
+					LogicalDeviceId:   "AOS-48x10_6x40-leaf_spine",
+					LinkPerSpineCount: 2,
+					LinkPerSpineSpeed: "10G",
+				},
+			},
+		},
+		"leaf_generic_with_tags": {
+			DisplayName:              "rdn " + testutils.RandString(5, "hex"),
+			FabricConnectivityDesign: enum.FabricConnectivityDesignL3Clos,
+			LeafSwitches: []apstra.RackElementLeafSwitchRequest{
+				{
+					Label:             leafLabel,
+					LogicalDeviceId:   "AOS-48x10_6x40-leaf_spine",
+					LinkPerSpineCount: 2,
+					LinkPerSpineSpeed: "10G",
+					Tags:              []apstra.ObjectId{"hypervisor", "bare_metal"},
+				},
+			},
+			GenericSystems: []apstra.RackElementGenericSystemRequest{
+				{
+					Count: 5,
+					Label: "some generic system",
+					Links: []apstra.RackLinkRequest{
+						{
+							Label:              "foo",
+							LinkPerSwitchCount: 1,
+							LinkSpeed:          "10G",
+							TargetSwitchLabel:  leafLabel,
+							AttachmentType:     apstra.RackLinkAttachmentTypeSingle,
+							LagMode:            apstra.RackLinkLagModeNone,
+							Tags:               []apstra.ObjectId{"firewall"},
+						},
+					},
+					LogicalDeviceId: "AOS-1x10-1",
+					Tags:            []apstra.ObjectId{"firewall"},
+				},
+			},
+			AccessSwitches: nil,
+		},
+	}
+
+	for _, client := range clients {
+		t.Run(client.Name(), func(t *testing.T) {
+			t.Parallel()
+			ctx = testutils.WrapCtxWithTestId(t, ctx)
+
+			for _, tCase := range testCases {
+				id, err := client.Client.CreateRackType(ctx, &tCase)
+				require.NoError(t, err)
+
+				rt, err := client.Client.GetRackType(ctx, id)
+				require.NoError(t, err)
+
+				require.Equal(t, rt.Id, id)
+				require.NotNil(t, rt.Data)
+				compare.RackType(t, tCase, *rt.Data)
+
+				err = client.Client.DeleteRackType(ctx, id)
+				require.NoError(t, err)
+			}
+		})
+	}
+}
---
+++ internal/test_utils/compare/rack_type.go
@@ -0,0 +1,48 @@
+// Copyright (c) Juniper Networks, Inc., 2025-2025.
+// All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package compare
+
+import (
+	"github.com/Juniper/apstra-go-sdk/apstra"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func RackType(t testing.TB, req apstra.RackTypeRequest, data apstra.RackTypeData) {
+	t.Helper()
+
+	require.Equal(t, req.Description, data.Description)
+	require.Equal(t, req.DisplayName, data.DisplayName)
+	require.Equal(t, req.FabricConnectivityDesign, data.FabricConnectivityDesign)
+}
+
+func rackElementLeafSwitch(t testing.TB, req apstra.RackElementLeafSwitchRequest, data apstra.RackElementLeafSwitch) {
+	t.Helper()
+
+	require.Equal(t, req.Label, data.Label)
+	mlagInfo(t, req.MlagInfo, data.MlagInfo)
+	require.Equal(t, req.LinkPerSpineCount, data.LinkPerSpineCount)
+	require.Equal(t, req.LinkPerSpineSpeed, data.LinkPerSpineSpeed)
+	require.Equal(t, req.RedundancyProtocol, data.RedundancyProtocol)
+	require.Equal(t, len(req.Tags), len(data.Tags))
+}
+
+func mlagInfo(t testing.TB, a, b *apstra.LeafMlagInfo) {
+	t.Helper()
+
+	if a == nil {
+		require.Nil(t, b)
+		return
+	}
+
+	require.NotNil(t, a)
+	require.NotNil(t, b)
+	require.Equal(t, a.LeafLeafL3LinkCount, b.LeafLeafL3LinkCount)
+	require.Equal(t, a.LeafLeafL3LinkPortChannelId, b.LeafLeafL3LinkPortChannelId)
+	require.Equal(t, a.LeafLeafL3LinkCount, b.LeafLeafL3LinkCount)
+	require.Equal(t, a.LeafLeafLinkPortChannelId, b.LeafLeafLinkPortChannelId)
+	require.Equal(t, a.LeafLeafLinkSpeed, b.LeafLeafLinkSpeed)
+	require.Equal(t, a.LeafLeafLinkSpeed, b.LeafLeafLinkSpeed)
+}
