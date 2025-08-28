53fa7551b420236802c4fa2c5c02e75cd66cad1f
Chris Marget <cmarget@juniper.net>
2025-08-28 16:18:26 -0400

    more template tests

--- apstra/api_design_templates_pod_based_integration_test.go
+++ apstra/api_design_templates_pod_based_integration_test.go
@@ -4,126 +4,108 @@
 
 //go:build integration
 
-package apstra
+package apstra_test
 
 import (
 	"context"
-	"log"
 	"testing"
+
+	"github.com/Juniper/apstra-go-sdk/apstra"
+	testutils "github.com/Juniper/apstra-go-sdk/internal/test_utils"
+	testclient "github.com/Juniper/apstra-go-sdk/internal/test_utils/test_client"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCreateGetDeletePodBasedTemplate(t *testing.T) {
-	ctx := context.Background()
+	ctx := testutils.WrapCtxWithTestId(t, context.Background())
+	clients := testclient.GetTestClients(t, ctx)
 
-	clients, err := getTestClients(ctx, t)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	dn := randString(5, "hex")
+	dn := testutils.RandString(5, "hex")
 
 	rbtdn := "rbtr-" + dn
-	rbtr := CreateRackBasedTemplateRequest{
+	rbtr := apstra.CreateRackBasedTemplateRequest{
 		DisplayName: rbtdn,
-		Spine: &TemplateElementSpineRequest{
+		Spine: &apstra.TemplateElementSpineRequest{
 			Count:                  2,
 			LinkPerSuperspineSpeed: "10G",
 			LogicalDevice:          "AOS-7x10-Spine",
 			LinkPerSuperspineCount: 1,
 			Tags:                   nil,
 		},
-		RackInfos: map[ObjectId]TemplateRackBasedRackInfo{
+		RackInfos: map[apstra.ObjectId]apstra.TemplateRackBasedRackInfo{
 			"access_switch": {
 				Count: 1,
 			},
 		},
-		DhcpServiceIntent:    &DhcpServiceIntent{Active: true},
-		AntiAffinityPolicy:   &AntiAffinityPolicy{Algorithm: AlgorithmHeuristic},
-		AsnAllocationPolicy:  &AsnAllocationPolicy{SpineAsnScheme: AsnAllocationSchemeSingle},
-		VirtualNetworkPolicy: &VirtualNetworkPolicy{},
+		DhcpServiceIntent:    &apstra.DhcpServiceIntent{Active: true},
+		AntiAffinityPolicy:   &apstra.AntiAffinityPolicy{Algorithm: apstra.AlgorithmHeuristic},
+		AsnAllocationPolicy:  &apstra.AsnAllocationPolicy{SpineAsnScheme: apstra.AsnAllocationSchemeSingle},
+		VirtualNetworkPolicy: &apstra.VirtualNetworkPolicy{},
 	}
 
-	for clientName, client := range clients {
-		log.Printf("testing CreateRackBasedTemplate() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
-		rbtid, err := client.client.CreateRackBasedTemplate(ctx, &rbtr)
-		if err != nil {
-			t.Fatal(err)
-		}
+	for _, client := range clients {
+		t.Run(client.Name(), func(t *testing.T) {
+			t.Parallel()
+			ctx := testutils.WrapCtxWithTestId(t, ctx)
 
-		pbtdn := "pbtr-" + dn
-		pbtr := CreatePodBasedTemplateRequest{
-			DisplayName: pbtdn,
-			Superspine: &TemplateElementSuperspineRequest{
-				PlaneCount:         1,
-				Tags:               nil,
-				SuperspinePerPlane: 4,
-				LogicalDeviceId:    "AOS-4x40_8x10-1",
-			},
-			PodInfos: map[ObjectId]TemplatePodBasedInfo{
-				rbtid: {
-					Count: 1,
+			rbtid, err := client.Client.CreateRackBasedTemplate(ctx, &rbtr)
+			require.NoError(t, err)
+
+			pbtdn := "pbtr-" + dn
+			pbtr := apstra.CreatePodBasedTemplateRequest{
+				DisplayName: pbtdn,
+				Superspine: &apstra.TemplateElementSuperspineRequest{
+					PlaneCount:         1,
+					Tags:               nil,
+					SuperspinePerPlane: 4,
+					LogicalDeviceId:    "AOS-4x40_8x10-1",
 				},
-			},
-			AntiAffinityPolicy: &AntiAffinityPolicy{
-				Algorithm:                AlgorithmHeuristic,
-				MaxLinksPerPort:          1,
-				MaxLinksPerSlot:          1,
-				MaxPerSystemLinksPerPort: 1,
-				MaxPerSystemLinksPerSlot: 1,
-				Mode:                     AntiAffinityModeDisabled,
-			},
-		}
+				PodInfos: map[apstra.ObjectId]apstra.TemplatePodBasedInfo{
+					rbtid: {
+						Count: 1,
+					},
+				},
+				AntiAffinityPolicy: &apstra.AntiAffinityPolicy{
+					Algorithm:                apstra.AlgorithmHeuristic,
+					MaxLinksPerPort:          1,
+					MaxLinksPerSlot:          1,
+					MaxPerSystemLinksPerPort: 1,
+					MaxPerSystemLinksPerSlot: 1,
+					Mode:                     apstra.AntiAffinityModeDisabled,
+				},
+			}
 
-		log.Printf("testing CreatePodBasedTemplate() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
-		pbtid, err := client.client.CreatePodBasedTemplate(ctx, &pbtr)
-		if err != nil {
-			t.Fatal(err)
-		}
+			pbtid, err := client.Client.CreatePodBasedTemplate(ctx, &pbtr)
+			require.NoError(t, err)
 
-		log.Printf("testing GetPodBasedTemplate() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
-		pbt, err := client.client.GetPodBasedTemplate(ctx, pbtid)
-		if err != nil {
-			t.Fatal(err)
-		}
+			pbt, err := client.Client.GetPodBasedTemplate(ctx, pbtid)
+			require.NoError(t, err)
 
-		if pbt.Data.DisplayName != pbtdn {
-			t.Fatalf("new template displayname mismatch: '%s' vs. '%s'", dn, pbt.Data.DisplayName)
-		}
+			require.Equal(t, pbtdn, pbt.Data.DisplayName)
 
-		log.Printf("testing DeleteTemplate() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
-		err = client.client.DeleteTemplate(ctx, pbtid)
-		if err != nil {
-			t.Fatal(err)
-		}
+			err = client.Client.DeleteTemplate(ctx, pbtid)
+			require.NoError(t, err)
 
-		log.Printf("testing DeleteTemplate() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
-		err = client.client.DeleteTemplate(ctx, rbtid)
-		if err != nil {
-			t.Fatal(err)
-		}
+			err = client.Client.DeleteTemplate(ctx, rbtid)
+			require.NoError(t, err)
+		})
 	}
 }
 
 func TestGetPodBasedTemplateByName(t *testing.T) {
-	ctx := context.Background()
-
-	clients, err := getTestClients(ctx, t)
-	if err != nil {
-		t.Fatal(err)
-	}
+	ctx := testutils.WrapCtxWithTestId(t, context.Background())
+	clients := testclient.GetTestClients(t, ctx)
 
 	name := "L2 superspine single plane"
 
 	for _, client := range clients {
-		pbt, err := client.client.GetPodBasedTemplateByName(ctx, name)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if pbt.templateType.String() != templateTypePodBased.string() {
-			t.Fatalf("expected '%s', got '%s'", pbt.templateType.String(), templateTypePodBased)
-		}
-		if pbt.Data.DisplayName != name {
-			t.Fatalf("expected '%s', got '%s'", name, pbt.Data.DisplayName)
-		}
+		t.Run(client.Name(), func(t *testing.T) {
+			t.Parallel()
+			ctx := testutils.WrapCtxWithTestId(t, ctx)
+
+			pbt, err := client.Client.GetPodBasedTemplateByName(ctx, name)
+			require.NoError(t, err)
+			require.Equal(t, name, pbt.Data.DisplayName)
+		})
 	}
 }
