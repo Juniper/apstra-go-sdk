84da200e84c8c94afcd7a288406d2b43d27bf050
Chris Marget <cmarget@juniper.net>
2025-08-28 22:33:48 -0400

    migrate `TestIbaPredefinedProbes()` to `apstra_test` package

--- apstra/api_iba_predefined_probes_test.go
+++ apstra/api_iba_predefined_probes_test.go
@@ -1,81 +1,74 @@
-// Copyright (c) Juniper Networks, Inc., 2023-2024.
+// Copyright (c) Juniper Networks, Inc., 2023-2025.
 // All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 //go:build integration
-// +build integration
 
-package apstra //
+package apstra_test
 
 import (
 	"context"
 	"encoding/json"
-	"fmt"
-	"log"
+	"github.com/Juniper/apstra-go-sdk/apstra"
+	testutils "github.com/Juniper/apstra-go-sdk/internal/test_utils"
+	dctestobj "github.com/Juniper/apstra-go-sdk/internal/test_utils/datacenter_test_objects"
+	testclient "github.com/Juniper/apstra-go-sdk/internal/test_utils/test_client"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestIbaPredefinedProbes(t *testing.T) {
-	ctx := context.Background()
-	clients, err := getTestClients(ctx, t)
-	require.NoError(t, err)
+	ctx := testutils.WrapCtxWithTestId(t, context.Background())
+	clients := testclient.GetTestClients(t, ctx)
 
-	for clientName, client := range clients {
-		clientName, client := clientName, client
-		t.Run(fmt.Sprintf("%s_%s", client.client.apiVersion, clientName), func(t *testing.T) {
+	expectedToFail := map[string]bool{
+		"external_ecmp_imbalance":            true,
+		"evpn_vxlan_type5":                   true,
+		"eastwest_traffic":                   true,
+		"vxlan_floodlist":                    true,
+		"fabric_hotcold_ifcounter":           true,
+		"specific_interface_flapping":        true,
+		"evpn_vxlan_type3":                   true,
+		"specific_hotcold_ifcounter":         true,
+		"spine_superspine_hotcold_ifcounter": true,
+	}
+
+	for _, client := range clients {
+		t.Run(client.Name(), func(t *testing.T) {
 			t.Parallel()
+			ctx := testutils.WrapCtxWithTestId(t, ctx)
 
-			log.Printf("testing Predefined Probes against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
-
-			bpClient := testBlueprintA(ctx, t, client.client)
+			bpClient := dctestobj.TestBlueprintA(t, ctx, client.Client)
 			pdps, err := bpClient.GetAllIbaPredefinedProbes(ctx)
 			require.NoError(t, err)
 
-			expectedToFail := map[string]bool{
-				"external_ecmp_imbalance":            true,
-				"evpn_vxlan_type5":                   true,
-				"eastwest_traffic":                   true,
-				"vxlan_floodlist":                    true,
-				"fabric_hotcold_ifcounter":           true,
-				"specific_interface_flapping":        true,
-				"evpn_vxlan_type3":                   true,
-				"specific_hotcold_ifcounter":         true,
-				"spine_superspine_hotcold_ifcounter": true,
-			}
 			t.Logf("Try an obviously fake name : %s", "FAKE")
 			_, err = bpClient.GetIbaPredefinedProbeByName(ctx, "FAKE")
-			if err == nil {
-				t.Fatal("FAKE name should have failed, but succeeded")
-			} else {
-				t.Log(err)
-			}
+			require.Error(t, err)
+			t.Log(err)
 
 			for _, p := range pdps {
 				t.Logf("Get Predefined Probe By Name %s", p.Name)
 				_, err := bpClient.GetIbaPredefinedProbeByName(ctx, p.Name)
-				if err != nil {
-					t.Fatal(err)
-				}
-				t.Log(p.Description)
-				t.Log(p.Schema)
+				require.NoError(t, err)
 
+				t.Log(p.Description)
+				t.Logf("%s", p.Schema)
 				t.Logf("Instantiating Probe %s", p.Name)
 
-				probeId, err := bpClient.InstantiateIbaPredefinedProbe(ctx, &IbaPredefinedProbeRequest{
+				probeId, err := bpClient.InstantiateIbaPredefinedProbe(ctx, &apstra.IbaPredefinedProbeRequest{
 					Name: p.Name,
-					Data: json.RawMessage([]byte(`{"label":"` + p.Name + `"}`)),
+					Data: json.RawMessage(`{"label":"` + p.Name + `"}`),
 				})
-				if err != nil {
-					if !expectedToFail[p.Name] {
-						t.Fatal(err)
-					} else {
-						t.Log(err)
-						t.Logf("%s was expected to fail", p.Name)
-						continue
-					}
+				if expectedToFail[p.Name] {
+					t.Log(err)
+					t.Logf("%s was expected to fail", p.Name)
+					continue
+				} else {
+					require.NoError(t, err)
 				}
+
 				t.Logf("Got back Probe Id %s \n Now Make a Widget with it.", probeId)
 			}
 		})
