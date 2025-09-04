3a7b8e404114c7fa7121e4925552def2bea38c8d
Chris Marget <cmarget@juniper.net>
2025-09-04 18:40:05 -0400

    migrate apstra/api_systems_test.go to apstra_test package

---
+++ apstra/api_systems_integration_test.go
@@ -0,0 +1,65 @@
+// Copyright (c) Juniper Networks, Inc., 2025-2025.
+// All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build integration
+
+package apstra_test
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	testutils "github.com/Juniper/apstra-go-sdk/internal/test_utils"
+	testclient "github.com/Juniper/apstra-go-sdk/internal/test_utils/test_client"
+	"github.com/stretchr/testify/require"
+)
+
+func TestListSystems(t *testing.T) {
+	ctx := testutils.WrapCtxWithTestId(t, context.Background())
+	clients := testclient.GetTestClients(t, ctx)
+
+	for _, client := range clients {
+		t.Run(client.Name(), func(t *testing.T) {
+			t.Parallel()
+			ctx := testutils.WrapCtxWithTestId(t, ctx)
+
+			systems, err := client.Client.ListSystems(ctx)
+			require.NoError(t, err)
+
+			for _, system := range systems {
+				log.Println(system)
+			}
+
+			systemInfos, err := client.Client.GetAllSystemsInfo(ctx)
+			require.NoError(t, err)
+			require.Equal(t, len(systems), len(systemInfos))
+			for _, systemInfo := range systemInfos {
+				require.Contains(t, systems, systemInfo.Id)
+			}
+		})
+	}
+}
+
+func TestGetSystems(t *testing.T) {
+	ctx := testutils.WrapCtxWithTestId(t, context.Background())
+	clients := testclient.GetTestClients(t, ctx)
+
+	for _, client := range clients {
+		t.Run(client.Name(), func(t *testing.T) {
+			t.Parallel()
+			ctx := testutils.WrapCtxWithTestId(t, ctx)
+
+			systems, err := client.Client.ListSystems(ctx)
+			require.NoError(t, err)
+
+			for _, s := range systems {
+				system, err := client.Client.GetSystemInfo(ctx, s)
+				require.NoError(t, err)
+
+				log.Println(system.Facts.HwModel)
+			}
+		})
+	}
+}
--- apstra/api_systems_test.go
+++ apstra/api_systems_test.go
@@ -1,84 +1,12 @@
-// Copyright (c) Juniper Networks, Inc., 2022-2024.
+// Copyright (c) Juniper Networks, Inc., 2022-2025.
 // All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 //go:build integration
-// +build integration
 
 package apstra
 
-import (
-	"context"
-	"log"
-	"testing"
-)
-
-func TestListSystems(t *testing.T) {
-	clients, err := getTestClients(context.Background(), t)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for clientName, client := range clients {
-		log.Printf("testing listSystems() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
-		systems, err := client.client.listSystems(context.TODO())
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		for _, s := range systems {
-			log.Println(s)
-		}
-	}
-}
-
-func TestGetAllSystems(t *testing.T) {
-	clients, err := getTestClients(context.Background(), t)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for clientName, client := range clients {
-		log.Printf("testing listSystems() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
-		systemIds, err := client.client.ListSystems(context.TODO())
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		log.Printf("testing getAllSystemsInfo() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
-		systems, err := client.client.GetAllSystemsInfo(context.TODO())
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if len(systemIds) != len(systems) {
-			t.Fatalf("system count discrepancy: %d vs. %d", len(systemIds), len(systems))
-		}
-	}
-}
-
-func TestGetSystems(t *testing.T) {
-	clients, err := getTestClients(context.Background(), t)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for clientName, client := range clients {
-		log.Printf("testing listSystems() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
-		systems, err := client.client.listSystems(context.TODO())
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		for _, s := range systems {
-			log.Printf("testing getSystemInfo() against %s %s (%s)", client.clientType, clientName, client.client.ApiVersion())
-			system, err := client.client.GetSystemInfo(context.TODO(), s)
-			if err != nil {
-				t.Fatal(err)
-			}
-			log.Println(system.Facts.HwModel)
-		}
-	}
-}
+import "testing"
 
 func TestSystemsStrings(t *testing.T) {
 	type apiStringIota interface {
