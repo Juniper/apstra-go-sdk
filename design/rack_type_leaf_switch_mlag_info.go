e86ab43b17f4b12897321fbb0f4f290b3320b191
Chris Marget <cmarget@juniper.net>
2025-09-22 19:05:44 -0400

    access switch support (no tests)

--- design/rack_type_leaf_switch_mlag_info.go
+++ design/rack_type_leaf_switch_mlag_info.go
@@ -9,15 +9,16 @@ import "github.com/Juniper/apstra-go-sdk/speed"
 var _ replicator[RackTypeLeafSwitchMLAGInfo] = (*RackTypeLeafSwitchMLAGInfo)(nil)
 
 type RackTypeLeafSwitchMLAGInfo struct {
-	LeafLeafL3LinkCount         int
-	LeafLeafL3LinkSpeed         speed.Speed
-	LeafLeafL3LinkPortChannelId int
-	LeafLeafLinkCount           int
-	LeafLeafLinkSpeed           speed.Speed
-	LeafLeafLinkPortChannelId   int
-	MLAGVLANID                  int
+	LeafLeafL3LinkCount         int         `json:"leaf_leaf_l3_link_count"`
+	LeafLeafL3LinkSpeed         speed.Speed `json:"leaf_leaf_l3_link_speed"`
+	LeafLeafL3LinkPortChannelId int         `json:"leaf_leaf_l3_link_port_channel_id"`
+	LeafLeafLinkCount           int         `json:"leaf_leaf_link_count"`
+	LeafLeafLinkSpeed           speed.Speed `json:"leaf_leaf_link_speed"`
+	LeafLeafLinkPortChannelId   int         `json:"leaf_leaf_link_port_channel_id"`
+	MLAGVLAN                    int         `json:"mlagvlan"`
 }
 
+// replicate returns a copy of itself with zero values for metadata fields
 func (r RackTypeLeafSwitchMLAGInfo) replicate() RackTypeLeafSwitchMLAGInfo {
 	return r
 }
