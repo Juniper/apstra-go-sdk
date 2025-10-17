29ce33ce99c77e8940e3994b77a0f9f782c71419
Chris Marget <cmarget@juniper.net>
2025-10-16 23:29:40 -0400

    change logical device unit test to use `design` rather than `design_test` package

--- design/logical_device_panel_test.go
+++ design/logical_device_panel_test.go
@@ -2,37 +2,36 @@
 // All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package design_test
+package design
 
 import (
 	"testing"
 
-	"github.com/Juniper/apstra-go-sdk/design"
 	"github.com/Juniper/apstra-go-sdk/enum"
 	"github.com/stretchr/testify/require"
 )
 
 func TestLogicalDevicePanel_MarshalUnmarshal(t *testing.T) {
 	type testCase struct {
-		panel design.LogicalDevicePanel
+		panel LogicalDevicePanel
 		json  string
 	}
 
-	var allRoles design.LogicalDevicePortRoles
+	var allRoles LogicalDevicePortRoles
 	allRoles.IncludeAllUses()
 
 	testCases := map[string]testCase{
 		"a": {
-			panel: design.LogicalDevicePanel{
-				PanelLayout: design.LogicalDevicePanelLayout{
+			panel: LogicalDevicePanel{
+				PanelLayout: LogicalDevicePanelLayout{
 					RowCount:    2,
 					ColumnCount: 4,
 				},
-				PortGroups: []design.LogicalDevicePanelPortGroup{
+				PortGroups: []LogicalDevicePanelPortGroup{
 					{
 						Count: 8,
 						Speed: "100G",
-						Roles: design.LogicalDevicePortRoles{enum.PortRoleSuperspine, enum.PortRoleLeaf},
+						Roles: LogicalDevicePortRoles{enum.PortRoleSuperspine, enum.PortRoleLeaf},
 					},
 				},
 				PortIndexing: enum.DesignLogicalDevicePanelPortIndexingTBLR,
@@ -40,21 +39,21 @@ func TestLogicalDevicePanel_MarshalUnmarshal(t *testing.T) {
 			json: `{"panel_layout":{"row_count":2,"column_count":4},"port_indexing":{"order":"T-B, L-R","schema":"absolute","start_index":1},"port_groups":[{"count":8,"speed":{"unit":"G","value":100},"roles":["superspine","leaf"]}]}`,
 		},
 		"b": {
-			panel: design.LogicalDevicePanel{
-				PanelLayout: design.LogicalDevicePanelLayout{
+			panel: LogicalDevicePanel{
+				PanelLayout: LogicalDevicePanelLayout{
 					RowCount:    4,
 					ColumnCount: 8,
 				},
-				PortGroups: []design.LogicalDevicePanelPortGroup{
+				PortGroups: []LogicalDevicePanelPortGroup{
 					{
 						Count: 8,
 						Speed: "100G",
-						Roles: design.LogicalDevicePortRoles{enum.PortRoleSuperspine, enum.PortRoleLeaf},
+						Roles: LogicalDevicePortRoles{enum.PortRoleSuperspine, enum.PortRoleLeaf},
 					},
 					{
 						Count: 24,
 						Speed: "25G",
-						Roles: design.LogicalDevicePortRoles{enum.PortRoleGeneric, enum.PortRoleAccess},
+						Roles: LogicalDevicePortRoles{enum.PortRoleGeneric, enum.PortRoleAccess},
 					},
 				},
 				PortIndexing: enum.DesignLogicalDevicePanelPortIndexingLRTB,
@@ -62,12 +61,12 @@ func TestLogicalDevicePanel_MarshalUnmarshal(t *testing.T) {
 			json: `{"panel_layout":{"row_count":4,"column_count":8},"port_indexing":{"order":"L-R, T-B","schema":"absolute","start_index":1},"port_groups":[{"count":8,"speed":{"unit":"G","value":100},"roles":["superspine","leaf"]},{"count":24,"speed":{"unit":"G","value":25},"roles":["generic","access"]}]}`,
 		},
 		"c": {
-			panel: design.LogicalDevicePanel{
-				PanelLayout: design.LogicalDevicePanelLayout{
+			panel: LogicalDevicePanel{
+				PanelLayout: LogicalDevicePanelLayout{
 					RowCount:    1,
 					ColumnCount: 1,
 				},
-				PortGroups: []design.LogicalDevicePanelPortGroup{
+				PortGroups: []LogicalDevicePanelPortGroup{
 					{
 						Count: 1,
 						Speed: "10M",
@@ -87,7 +86,7 @@ func TestLogicalDevicePanel_MarshalUnmarshal(t *testing.T) {
 			jsonResult, err := tCase.panel.MarshalJSON()
 			require.NoError(t, err)
 
-			var panelResult design.LogicalDevicePanel
+			var panelResult LogicalDevicePanel
 			err = panelResult.UnmarshalJSON([]byte(tCase.json))
 			require.NoError(t, err)
 
