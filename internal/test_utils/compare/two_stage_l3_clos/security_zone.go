f85fd6666c32b135ceac577a8d92467a1153c1c5
Chris Marget <cmarget@juniper.net>
2026-01-06 08:16:55 -0500

    use `msg` when invoking `require` functions

--- internal/test_utils/compare/two_stage_l3_clos/security_zone.go
+++ internal/test_utils/compare/two_stage_l3_clos/security_zone.go
@@ -18,56 +18,56 @@ func SecurityZone(t testing.TB, req, resp apstra.SecurityZone, msg ...string) {
 	msg = testmessage.Add(msg, "Comparing Security Zone")
 
 	if req.ID() != nil {
-		require.NotNil(t, resp.ID())
-		require.Equal(t, *req.ID(), *resp.ID())
+		require.NotNil(t, resp.ID(), msg)
+		require.Equal(t, *req.ID(), *resp.ID(), msg)
 	}
 
 	require.Equal(t, req.Label, resp.Label, msg)
 
 	if req.Description == nil {
-		require.Nil(t, resp.Description)
+		require.Nil(t, resp.Description, msg)
 	} else {
-		require.NotNil(t, resp.Description)
-		require.Equal(t, *req.Description, *resp.Description)
+		require.NotNil(t, resp.Description, msg)
+		require.Equal(t, *req.Description, *resp.Description, msg)
 	}
 
-	require.Equal(t, req.Description, resp.Description, msg)
-	require.Equal(t, req.Type, resp.Type, msg)
+	require.Equal(t, req.Description, resp.Description, msg, msg)
+	require.Equal(t, req.Type, resp.Type, msg, msg)
 
-	require.Equal(t, req.VRFName, resp.VRFName, msg)
+	require.Equal(t, req.VRFName, resp.VRFName, msg, msg)
 	if req.RoutingPolicyID != "" {
 		require.Equal(t, req.RoutingPolicyID, resp.RoutingPolicyID, msg)
 	}
 
 	if req.RTPolicy == nil {
-		require.Nil(t, resp.RTPolicy)
+		require.Nil(t, resp.RTPolicy, msg)
 	} else {
-		require.NotNil(t, resp.RTPolicy)
-		require.Equal(t, *req.RTPolicy, *resp.RTPolicy)
+		require.NotNil(t, resp.RTPolicy, msg)
+		require.Equal(t, *req.RTPolicy, *resp.RTPolicy, msg)
 	}
 
 	if req.VLAN != nil {
-		require.NotNil(t, resp.VLAN)
-		require.Equal(t, *req.VLAN, *resp.VLAN)
+		require.NotNil(t, resp.VLAN, msg)
+		require.Equal(t, *req.VLAN, *resp.VLAN, msg)
 	}
 
 	if req.VNI != nil {
-		require.NotNil(t, resp.VNI)
-		require.Equal(t, *req.VNI, *resp.VNI)
+		require.NotNil(t, resp.VNI, msg)
+		require.Equal(t, *req.VNI, *resp.VNI, msg)
 	}
 
 	if req.JunosEVPNIRBMode != nil {
-		require.NotNil(t, resp.JunosEVPNIRBMode)
+		require.NotNil(t, resp.JunosEVPNIRBMode, msg)
 		require.Equal(t, *req.JunosEVPNIRBMode, *resp.JunosEVPNIRBMode, msg)
 	}
 
 	if req.AddressingSupport != nil {
-		require.NotNil(t, resp.AddressingSupport)
-		require.Equal(t, *req.AddressingSupport, *resp.AddressingSupport)
+		require.NotNil(t, resp.AddressingSupport, msg)
+		require.Equal(t, *req.AddressingSupport, *resp.AddressingSupport, msg)
 	}
 
 	if req.DisableIPv4 != nil {
-		require.NotNil(t, resp.DisableIPv4)
-		require.Equal(t, *req.DisableIPv4, *resp.DisableIPv4)
+		require.NotNil(t, resp.DisableIPv4, msg)
+		require.Equal(t, *req.DisableIPv4, *resp.DisableIPv4, msg)
 	}
 }
