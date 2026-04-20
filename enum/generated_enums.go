2df336ca9b55c211faad83b0f1753657b7145703
Chris Marget <cmarget@juniper.net>
2026-04-20 19:13:36 -0400

    WIP: Switching Zone

--- enum/generated_enums.go
+++ enum/generated_enums.go
@@ -1625,6 +1625,68 @@ func (o *SviIpv6Mode) UnmarshalJSON(bytes []byte) error {
 }
 
 var (
+	_ enum             = (*SwitchingZoneImplementationType)(nil)
+	_ json.Marshaler   = (*SwitchingZoneImplementationType)(nil)
+	_ json.Unmarshaler = (*SwitchingZoneImplementationType)(nil)
+)
+
+func (o SwitchingZoneImplementationType) String() string {
+	return o.Value
+}
+
+func (o *SwitchingZoneImplementationType) FromString(s string) error {
+	if SwitchingZoneImplementationTypes.Parse(s) == nil {
+		return newEnumParseError(o, s)
+	}
+	o.Value = s
+	return nil
+}
+
+func (o SwitchingZoneImplementationType) MarshalJSON() ([]byte, error) {
+	return json.Marshal(o.String())
+}
+
+func (o *SwitchingZoneImplementationType) UnmarshalJSON(bytes []byte) error {
+	var s string
+	err := json.Unmarshal(bytes, &s)
+	if err != nil {
+		return err
+	}
+	return o.FromString(s)
+}
+
+var (
+	_ enum             = (*SwitchingZoneMACVRFServiceType)(nil)
+	_ json.Marshaler   = (*SwitchingZoneMACVRFServiceType)(nil)
+	_ json.Unmarshaler = (*SwitchingZoneMACVRFServiceType)(nil)
+)
+
+func (o SwitchingZoneMACVRFServiceType) String() string {
+	return o.Value
+}
+
+func (o *SwitchingZoneMACVRFServiceType) FromString(s string) error {
+	if SwitchingZoneMACVRFServiceTypes.Parse(s) == nil {
+		return newEnumParseError(o, s)
+	}
+	o.Value = s
+	return nil
+}
+
+func (o SwitchingZoneMACVRFServiceType) MarshalJSON() ([]byte, error) {
+	return json.Marshal(o.String())
+}
+
+func (o *SwitchingZoneMACVRFServiceType) UnmarshalJSON(bytes []byte) error {
+	var s string
+	err := json.Unmarshal(bytes, &s)
+	if err != nil {
+		return err
+	}
+	return o.FromString(s)
+}
+
+var (
 	_ enum             = (*SystemManagementLevel)(nil)
 	_ json.Marshaler   = (*SystemManagementLevel)(nil)
 	_ json.Unmarshaler = (*SystemManagementLevel)(nil)
@@ -2290,6 +2352,18 @@ var (
 		SviIpv6ModeEnabled,
 		SviIpv6ModeForced,
 		SviIpv6ModeLinkLocal,
+	)
+
+	_                                enum = new(SwitchingZoneImplementationType)
+	SwitchingZoneImplementationTypes      = oenum.New(
+		SwitchingZoneImplementationTypeDefault,
+		SwitchingZoneImplementationTypeMACVRF,
+	)
+
+	_                               enum = new(SwitchingZoneMACVRFServiceType)
+	SwitchingZoneMACVRFServiceTypes      = oenum.New(
+		SwitchingZoneMACVRFServiceTypeVLANAware,
+		SwitchingZoneMACVRFServiceTypeVLANBundle,
 	)
 
 	_                      enum = new(SystemManagementLevel)
