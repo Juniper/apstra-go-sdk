813f07aba4b89258ff141645e4bc439ee3fe01f5
Chris Marget <cmarget@juniper.net>
2026-06-10 22:25:58 -0400

    revert comment-only change

--- apstra/two_stage_l3_clos_client.go
+++ apstra/two_stage_l3_clos_client.go
@@ -678,8 +678,8 @@ func (o *TwoStageL3ClosClient) SetFabricSettings(ctx context.Context, in *Fabric
 	}
 
 	if in.Ipv6Enabled != nil && !compatibility.FabricSettingsIPv6EnabledOK.Check(o.client.apiVersion) {
-		// Beginning with Apstra 6.1.0, IPv6Enabled is no longer a fabric-wide setting and it must not be sent.
-		// Make a copy of the caller's struct and clear the IPv6Enabled pointer.
+		// Beginning with Apstra 6.1.0, Ipv6Enabled is no longer a fabric-wide setting and it must not be sent.
+		// Make a copy of the caller's struct and clear the Itpv6Enabled pointer.
 		in = toPtr(*in)
 		in.Ipv6Enabled = nil
 	}
